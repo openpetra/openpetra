4871a1a1ac9920806f8ff165686fffee566cd090
Timotheus Pokorra <pokorra@users.sourceforge.net>
2009-06-22 17:50:51 +0200

    work on postgresql demo database create a backup of the database, and deliver it, and restore on linux

---
+++ setup/petra0300/linuxserver/initpg.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# you have to run this as user postgres
+# eg. su postgres -c initpg.sh
+dropdb petra; 
+psql -c "DROP OWNED BY petraserver; DROP USER IF EXISTS petraserver; CREATE USER petraserver PASSWORD 'TOBESETBYINSTALLER'; CREATE USER TIMOP PASSWORD 'TOBESETBYINSTALLER'; "
+createdb -T template0 petra
+pg_restore -d petra db30/demo-pg.bak
