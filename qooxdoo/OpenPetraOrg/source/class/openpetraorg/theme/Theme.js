5943b98d37ce53b79d6527dc42d4f474c0c8dbe1
Timotheus Pokorra <pokorra@users.sourceforge.net>
2009-07-12 01:48:01 +0200

    qooxdoo client (javascript) that uses the soap web service for login

---
+++ qooxdoo/OpenPetraOrg/source/class/openpetraorg/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("openpetraorg.theme.Theme",
+{
+  meta :
+  {
+    color : openpetraorg.theme.Color,
+    decoration : openpetraorg.theme.Decoration,
+    font : openpetraorg.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : openpetraorg.theme.Appearance
+  }
+});
