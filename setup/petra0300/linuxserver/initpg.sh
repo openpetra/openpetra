3a7f44434b141f022a35aa9265a7ca3cba501768
Timotheus Pokorra <pokorra@users.sourceforge.net>
2009-07-13 18:51:36 +0200

    linux adjustments
    fix some problems with building the linux installer
    start server and client with nant on linux

--- setup/petra0300/linuxserver/initpg.sh
+++ setup/petra0300/linuxserver/initpg.sh
@@ -2,6 +2,6 @@
 # you have to run this as user postgres
 # eg. su postgres -c initpg.sh
 dropdb petra; 
-psql -c "DROP OWNED BY petraserver; DROP USER IF EXISTS petraserver; CREATE USER petraserver PASSWORD 'TOBESETBYINSTALLER'; CREATE USER TIMOP PASSWORD 'TOBESETBYINSTALLER'; "
+psql -c "DROP OWNED BY petraserver; DROP USER IF EXISTS petraserver; CREATE USER petraserver PASSWORD 'TOBESETBYINSTALLER'; CREATE USER timop PASSWORD 'TOBESETBYINSTALLER'; "
 createdb -T template0 petra
 pg_restore -d petra db30/demo-pg.bak
