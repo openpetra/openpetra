4d0e100fb67aafbc50a47e1952bcd2f6431fcba4
unknown <timotheus@.(none)>
2009-10-23 16:24:29 +0200

    add balance sheet
    initial version
    no selection of columns yet

---
+++ csharp/ICT/Petra/Client/lib/MReporting/gui/MFinance/BalanceSheet.ManualCode.cs
@@ -0,0 +1,64 @@
+﻿/*************************************************************************
+ *
+ * DO NOT REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * @Authors:
+ *       timop
+ *
+ * Copyright 2004-2009 by OM International
+ *
+ * This file is part of OpenPetra.org.
+ *
+ * OpenPetra.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenPetra.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenPetra.org.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ************************************************************************/
+using System;
+using Ict.Petra.Client.MFinance.Logic;
+using Ict.Petra.Client.MReporting.Logic;
+
+namespace Ict.Petra.Client.MReporting.Gui.MFinance
+{
+    public partial class TFrmBalanceSheet
+    {
+        private Int32 FLedgerNumber;
+
+        /// <summary>
+        /// the report should be run for this ledger
+        /// </summary>
+        public Int32 LedgerNumber
+        {
+            set
+            {
+                FLedgerNumber = value;
+
+                TFinanceControls.InitialiseCostCentreList(ref clbCostCentres, FLedgerNumber, true, false, false, false);
+                TFinanceControls.InitialiseAccountHierarchyList(ref cmbAccountHierarchy, FLedgerNumber);
+                TFinanceControls.InitialiseAvailableFinancialYearsList(ref cmbPeriodYear, FLedgerNumber);
+
+                txtLedger.Text = TFinanceControls.GetLedgerNumberAndName(FLedgerNumber);
+
+                // if there is only one hierarchy, disable the control
+                this.cmbAccountHierarchy.Enabled = (this.cmbAccountHierarchy.Count > 1);
+                clbCostCentres.SetCheckedStringList("");
+
+                FPetraUtilsObject.LoadDefaultSettings();
+            }
+        }
+
+        private void ReadControlsManual(TRptCalculator ACalc)
+        {
+            ACalc.AddParameter("param_ledger_number_i", FLedgerNumber);
+        }
+    }
+}
