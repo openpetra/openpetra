79322513df909083514490a858756c83bd01707b
Timotheus Pokorra <pokorra@users.sourceforge.net>
2010-01-09 10:23:44 +0100

    new start and stop script for init.d

--- setup/petra0300/linuxserver/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd bin30
-touch ../log30/Server.log
-mono --server PetraServerConsole.exe -C:../etc30/PetraServerConsole.config -RunWithoutMenu:true &> /dev/null &
-# in order to see if the server started successfully, wait a few seconds and then show the end of the log file
-sleep 5
-tail ../log30/Server.log
-cd ..
