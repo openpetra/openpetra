10d8cf88c8c24ed1aa74c0287094225fa353bf93
Timotheus Pokorra <timotheus.pokorra@gmail.com>
2010-04-21 23:46:08 +0200

    special partner import
    this will be a powerful import of data related to persons

---
+++ csharp/ICT/Petra/Client/lib/MPartner/gui/PartnerImport.ManualCode.cs
@@ -0,0 +1,63 @@
+﻿/*************************************************************************
+ *
+ * DO NOT REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * @Authors:
+ *       timop
+ *
+ * Copyright 2004-2010 by OM International
+ *
+ * This file is part of OpenPetra.org.
+ *
+ * OpenPetra.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenPetra.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenPetra.org.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ************************************************************************/
+using System;
+using System.Xml;
+using System.Windows.Forms;
+using Ict.Common;
+using Ict.Common.IO;
+using Ict.Petra.Shared.MPartner;
+using Ict.Petra.Shared.MPartner.Partner.Data;
+using Ict.Petra.Client.App.Gui;
+
+namespace Ict.Petra.Client.MPartner.Gui
+{
+    public partial class TFrmPartnerImport
+    {
+        /// <summary>
+        /// todoComment
+        /// </summary>
+        public bool SaveChanges()
+        {
+            // TODO
+            return false;
+        }
+
+        /// <summary>
+        /// todoComment
+        /// </summary>
+        public void FileSave(System.Object sender, EventArgs e)
+        {
+            // TODO
+        }
+
+        XmlNode FCurrentNode = null;
+        private void OpenFile(System.Object sender, EventArgs e)
+        {
+            // TODO: tell the parser which CSV delimiter to use, cmbDelimiter
+            XmlDocument doc = TImportExportDialogs.ImportWithDialog(Catalog.GetString("Select the file for importing partners"));
+        }
+    }
+}
---
+++ csharp/ICT/Petra/Client/lib/MPartner/gui/PartnerImport.cs
@@ -0,0 +1,193 @@
+/* auto generated with nant generateWinforms from PartnerImport.yaml
+ *
+ * DO NOT edit manually, DO NOT edit with the designer
+ * use a user control if you need to modify the screen content
+ *
+ */
+/*************************************************************************
+ *
+ * DO NOT REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * @Authors:
+ *       auto generated
+ *
+ * Copyright 2004-2009 by OM International
+ *
+ * This file is part of OpenPetra.org.
+ *
+ * OpenPetra.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenPetra.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenPetra.org.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ************************************************************************/
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+using Ict.Petra.Shared;
+using System.Resources;
+using System.Collections.Specialized;
+using Mono.Unix;
+using Ict.Common;
+using Ict.Petra.Client.App.Core;
+using Ict.Petra.Client.App.Core.RemoteObjects;
+using Ict.Common.Controls;
+using Ict.Petra.Client.CommonForms;
+
+namespace Ict.Petra.Client.MPartner.Gui
+{
+
+  /// auto generated: Import Partners
+  public partial class TFrmPartnerImport: System.Windows.Forms.Form, IFrmPetraEdit
+  {
+    private TFrmPetraEditUtils FPetraUtilsObject;
+
+    /// constructor
+    public TFrmPartnerImport(IntPtr AParentFormHandle) : base()
+    {
+      //
+      // Required for Windows Form Designer support
+      //
+      InitializeComponent();
+      #region CATALOGI18N
+
+      // this code has been inserted by GenerateI18N, all changes in this region will be overwritten by GenerateI18N
+      this.lblFilename.Text = Catalog.GetString("Filename:");
+      this.btnSelectFile.Text = Catalog.GetString("Select File");
+      this.lblDelimiter.Text = Catalog.GetString("Delimiter:");
+      this.lblExplanation.Text = Catalog.GetString("Explanation:");
+      this.btnSkip.Text = Catalog.GetString("Skip");
+      this.btnCreateNewFamilyAndPerson.Text = Catalog.GetString("Create New Family And Person");
+      this.btnFindOtherFamily.Text = Catalog.GetString("Find Other Family");
+      this.chkReplaceAddress.Text = Catalog.GetString("Replace Address");
+      this.tbbSave.ToolTipText = Catalog.GetString("Saves changed data");
+      this.tbbSave.Text = Catalog.GetString("&Save");
+      this.tbbStartImport.Text = Catalog.GetString("Start Import");
+      this.tbbCancelImport.Text = Catalog.GetString("Cancel Import");
+      this.mniFileSave.ToolTipText = Catalog.GetString("Saves changed data");
+      this.mniFileSave.Text = Catalog.GetString("&Save");
+      this.mniFilePrint.Text = Catalog.GetString("&Print...");
+      this.mniClose.ToolTipText = Catalog.GetString("Closes this window");
+      this.mniClose.Text = Catalog.GetString("&Close");
+      this.mniFile.Text = Catalog.GetString("&File");
+      this.mniEditUndoCurrentField.Text = Catalog.GetString("Undo &Current Field");
+      this.mniEditUndoScreen.Text = Catalog.GetString("&Undo Screen");
+      this.mniEditFind.Text = Catalog.GetString("&Find...");
+      this.mniEdit.Text = Catalog.GetString("&Edit");
+      this.mniHelpPetraHelp.Text = Catalog.GetString("&Petra Help");
+      this.mniHelpBugReport.Text = Catalog.GetString("Bug &Report");
+      this.mniHelpAboutPetra.Text = Catalog.GetString("&About Petra");
+      this.mniHelpDevelopmentTeam.Text = Catalog.GetString("&The Development Team...");
+      this.mniHelp.Text = Catalog.GetString("&Help");
+      this.Text = Catalog.GetString("Import Partners");
+      #endregion
+
+      FPetraUtilsObject = new TFrmPetraEditUtils(AParentFormHandle, this, stbMain);
+      FPetraUtilsObject.ActionEnablingEvent += ActionEnabledEvent;
+
+      FPetraUtilsObject.InitActionState();
+
+    }
+
+    private void TFrmPetra_Activated(object sender, EventArgs e)
+    {
+        FPetraUtilsObject.TFrmPetra_Activated(sender, e);
+    }
+
+    private void TFrmPetra_Load(object sender, EventArgs e)
+    {
+        FPetraUtilsObject.TFrmPetra_Load(sender, e);
+    }
+
+    private void TFrmPetra_Closing(object sender, CancelEventArgs e)
+    {
+        FPetraUtilsObject.TFrmPetra_Closing(sender, e);
+    }
+
+    private void Form_KeyDown(object sender, KeyEventArgs e)
+    {
+        FPetraUtilsObject.Form_KeyDown(sender, e);
+    }
+
+    private void TFrmPetra_Closed(object sender, EventArgs e)
+    {
+        // TODO? Save Window position
+    }
+
+#region Implement interface functions
+
+    /// auto generated
+    public void RunOnceOnActivation()
+    {
+    }
+
+    /// <summary>
+    /// Adds event handlers for the appropiate onChange event to call a central procedure
+    /// </summary>
+    public void HookupAllControls()
+    {
+    }
+
+    /// auto generated
+    public void HookupAllInContainer(Control container)
+    {
+        FPetraUtilsObject.HookupAllInContainer(container);
+    }
+
+    /// auto generated
+    public bool CanClose()
+    {
+        return FPetraUtilsObject.CanClose();
+    }
+
+    /// auto generated
+    public TFrmPetraUtils GetPetraUtilsObject()
+    {
+        return (TFrmPetraUtils)FPetraUtilsObject;
+    }
+#endregion
+
+#region Action Handling
+
+    /// auto generated
+    public void ActionEnabledEvent(object sender, ActionEventArgs e)
+    {
+        if (e.ActionName == "actSave")
+        {
+            tbbSave.Enabled = e.Enabled;
+            mniFileSave.Enabled = e.Enabled;
+        }
+        if (e.ActionName == "actClose")
+        {
+            mniClose.Enabled = e.Enabled;
+        }
+        mniFilePrint.Enabled = false;
+        mniEditUndoCurrentField.Enabled = false;
+        mniEditUndoScreen.Enabled = false;
+        mniEditFind.Enabled = false;
+        mniHelpPetraHelp.Enabled = false;
+        mniHelpBugReport.Enabled = false;
+        mniHelpAboutPetra.Enabled = false;
+        mniHelpDevelopmentTeam.Enabled = false;
+    }
+
+    /// auto generated
+    protected void actClose(object sender, EventArgs e)
+    {
+        FPetraUtilsObject.ExecuteAction(eActionId.eClose);
+    }
+
+#endregion
+  }
+}
