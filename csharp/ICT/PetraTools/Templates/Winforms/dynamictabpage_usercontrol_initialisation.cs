089bf5b7068c1ad75be4d35946dcc8308eb96e47
christiank <christian.kendel@ict.om.org>
2010-06-15 14:13:54 +0100

    *WinForms GUI generator* Ensure controls on dynamically loaded UserControls scale correctly on "Large Fonts (120DPI)" display setting.

--- csharp/ICT/PetraTools/Templates/Winforms/dynamictabpage_usercontrol_initialisation.cs
+++ csharp/ICT/PetraTools/Templates/Winforms/dynamictabpage_usercontrol_initialisation.cs
@@ -22,7 +22,16 @@ if ({#TABCONTROLNAME}.SelectedTab == {#CONTROLNAME})
         this.Cursor = Cursors.Default;
     }
     else
-    {
+    {    
         OnTabPageEvent(new TTabPageEventArgs({#CONTROLNAME}, FUco{#CONTROLNAMEWITHOUTPREFIX}, "SubsequentActivation"));
+        
+        /*
+         * The following command seems strange and unnecessary; however, it is necessary
+         * to make things scale correctly on "Large Fonts (120DPI)" display setting.
+         */
+        if (TClientSettings.GUIRunningOnNonStandardDPI)
+        {
+            FUco{#CONTROLNAMEWITHOUTPREFIX}.AdjustAfterResizing();
+        }        
     }
 }
