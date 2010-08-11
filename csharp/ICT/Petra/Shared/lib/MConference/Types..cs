c6d66ae7bfbd995b71eaa123e487a9a84db9ae60
berndr <bernd.rehberger@ict.om.org>
2010-08-11 11:06:56 +0200

    Add the single .cs files for the field reports. Add the Types..cs. Add the standard setting files for the field reports.

---
+++ csharp/ICT/Petra/Shared/lib/MConference/Types..cs
@@ -0,0 +1,47 @@
+﻿//
+// DO NOT REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+//
+// @Authors:
+//       berndr
+//
+// Copyright 2004-2010 by OM International
+//
+// This file is part of OpenPetra.org.
+//
+// OpenPetra.org is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenPetra.org is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenPetra.org.  If not, see <http://www.gnu.org/licenses/>.
+//
+using System;
+
+namespace Ict.Petra.Shared.MConference
+{
+    /// <summary>
+    /// Defines the types of units that should be returned to the conference field reports.
+    /// </summary>
+    public enum TUnitTypeEnum
+    {
+        /// <summary>Fields that are charged for the conferencee</summary>
+        utChargedFields,
+        /// <summary>Fields that send people to the conference</summary>
+        utSendingFields,
+        /// <summary>Fields that receive people from the conference</summary>
+        utReceivingFields,
+        /// <summary>Fields where applicants from the conference are registered</summary>
+        utRegisteringFields,
+        /// <summary>All the campaign options of this conference</summary>
+        utCampaignOptions,
+
+        /// <summary>Undefined or unknown type </summary>
+        utUnknown
+    }
+}
