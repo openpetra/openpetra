9fbf3366ab9c0a5fb7b4eaeb09869b5c906d19de
Timotheus Pokorra <timotheus.pokorra@gmail.com>
2010-08-16 21:28:13 +0200

    add missing file to previous commit
    loading only some gl batches

---
+++ csharp/ICT/Petra/Shared/lib/MFinance/Types.cs
@@ -0,0 +1,58 @@
+﻿//
+// DO NOT REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+//
+// @Authors:
+//       timop
+//
+// Copyright 2004-2010 by OM International
+//
+// This file is part of OpenPetra.org.
+//
+// OpenPetra.org is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenPetra.org is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenPetra.org.  If not, see <http://www.gnu.org/licenses/>.
+//
+using System;
+
+namespace Ict.Petra.Shared.MFinance
+{
+    /// <summary>
+    /// enumeration for the filter of the batch screens.
+    /// </summary>
+    public enum TFinanceBatchFilterEnum
+    {
+        /// <summary>
+        /// no batches displayed
+        /// </summary>
+        fbfNone = 0,
+
+        /// <summary>
+        /// show only batches that are ready for posting
+        /// </summary>
+        fbfReadyForPosting = 1,
+
+        /// <summary>
+        /// show batches that are unposted and not cancelled. includes ready for posting
+        /// </summary>
+        fbfEditing = 3,
+
+        /// <summary>
+        /// show all batches in current and forward posting periods. includes editing and batches ready for posting
+        /// </summary>
+        fbfAllCurrent = 7,
+
+        /// <summary>
+        /// show all batches, even previous periods (and years?). includes editing and batches ready for posting
+        /// </summary>
+        fbfAll = 15,
+    };
+}
