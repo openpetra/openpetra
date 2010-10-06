6d9dab3ddfebecd238a7ca04a0e108eb235e07b5
berndr <bernd.rehberger@ict.om.org>
2010-10-06 11:39:34 +0200

    Add report "One year monthly giving"

---
+++ csharp/ICT/Petra/Client/lib/MReporting/gui/MFinance/OneYearMonthlyGiving.ManualCode.cs
@@ -0,0 +1,115 @@
+//
+// DO NOT REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+//
+// @Authors:
+//       berndr
+//
+// Copyright 2004-2010 by OM International
+//
+// This file is part of OpenPetra.org.
+//
+// OpenPetra.org is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenPetra.org is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenPetra.org.  If not, see <http://www.gnu.org/licenses/>.
+//
+using System;
+using Ict.Common.Verification;
+using Ict.Petra.Client.MFinance.Logic;
+using Ict.Petra.Client.MReporting.Logic;
+using Ict.Petra.Client.App.Core.RemoteObjects;
+using Ict.Petra.Shared.MReporting;
+using Mono.Unix;
+
+namespace Ict.Petra.Client.MReporting.Gui.MFinance
+{
+    public partial class TFrmOneYearMonthlyGiving
+    {
+        private Int32 FLedgerNumber;
+
+        /// <summary>
+        /// the report should be run for this ledger
+        /// </summary>
+        public Int32 LedgerNumber
+        {
+            set
+            {
+                FLedgerNumber = value;
+                lblLedger.Text = Catalog.GetString("Ledger: ") + FLedgerNumber.ToString();
+            }
+        }
+
+        private void ReadControlsManual(TRptCalculator ACalc, TReportActionEnum AReportAction)
+        {
+            if ((AReportAction == TReportActionEnum.raGenerate)
+                && (rbtPartner.Checked && (txtRecipient.Text == "0000000000")))
+            {
+                TVerificationResult VerificationResult = new TVerificationResult(
+                    Catalog.GetString("No donor selected."),
+                    Catalog.GetString("Please select a donor."),
+                    TResultSeverity.Resv_Critical);
+
+                FPetraUtilsObject.AddVerificationResult(VerificationResult);
+            }
+
+            if ((AReportAction == TReportActionEnum.raGenerate)
+                && rbtExtract.Checked
+                && (txtExtract.Text == ""))
+            {
+                TVerificationResult VerificationMessage = new TVerificationResult(
+                    Catalog.GetString("Enter a extract name."),
+                    Catalog.GetString("No extract name entered!"), TResultSeverity.Resv_Critical);
+                FPetraUtilsObject.AddVerificationResult(VerificationMessage);
+            }
+
+            if ((AReportAction == TReportActionEnum.raGenerate)
+                && (dtpFromDate.Date > dtpToDate.Date))
+            {
+                TVerificationResult VerificationResult = new TVerificationResult(
+                    Catalog.GetString("From date is later than to date."),
+                    Catalog.GetString("Please change from date or to date."),
+                    TResultSeverity.Resv_Critical);
+                FPetraUtilsObject.AddVerificationResult(VerificationResult);
+            }
+
+            if ((AReportAction == TReportActionEnum.raGenerate)
+                && (dtpFromDate.Date.Value.Year != dtpToDate.Date.Value.Year))
+            {
+                TVerificationResult VerificationResult = new TVerificationResult(
+                    Catalog.GetString("Year value in from-date is different than from year value in to-date."),
+                    Catalog.GetString("Please use the same year."),
+                    TResultSeverity.Resv_Critical);
+                FPetraUtilsObject.AddVerificationResult(VerificationResult);
+            }
+
+            ACalc.AddParameter("param_ledger_number_i", FLedgerNumber);
+            ACalc.AddParameter("param_recipient_key", txtRecipient.Text);
+            ACalc.AddParameter("param_extract_name", txtExtract.Text);
+
+            ACalc.AddParameter("param_year", dtpFromDate.Date.Value.Year);
+            ACalc.AddParameter("param_months", dtpToDate.Date.Value.Month - dtpFromDate.Date.Value.Month + 1);
+
+            int MaxColumns = ACalc.GetParameters().Get("MaxDisplayColumns").ToInt();
+
+            for (int Counter = 0; Counter <= MaxColumns; ++Counter)
+            {
+                String ColumnName = ACalc.GetParameters().Get("param_calculation", Counter, 0).ToString();
+                ACalc.AddParameter(ColumnName, Counter);
+            }
+        }
+
+        private void SetControlsManual(TParameterList AParameters)
+        {
+            txtRecipient.Text = AParameters.Get("param_recipient_key").ToString();
+            txtExtract.Text = AParameters.Get("param_extract_name").ToString();
+        }
+    }
+}
