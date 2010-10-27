327b22ac0862d4648fcf368fec1c196b6935fa8a
berndr <bernd.rehberger@ict.om.org>
2010-10-27 12:12:15 +0200

    uncrustify

--- csharp/ICT/PetraTools/FixProjectFiles/FixProjectReferences.cs
+++ csharp/ICT/PetraTools/FixProjectFiles/FixProjectReferences.cs
@@ -279,22 +279,22 @@ public class TFixProjectReferences : TCSProjTools
                     {
                         // check whether we should have a ProjectReference instead to help with the build order
                         string referencedDll = child2.Attributes["Include"].Value;
-                        
+
                         if (referencedDll == "Mono.Posix")
                         {
-                        	child2.Attributes["Include"].Value = "GNU.Gettext";
-                        	referencedDll = child2.Attributes["Include"].Value;
+                            child2.Attributes["Include"].Value = "GNU.Gettext";
+                            referencedDll = child2.Attributes["Include"].Value;
                         }
 
                         if ((child2.FirstChild != null) && (child2.FirstChild.Name == "HintPath"))
                         {
                             string hintPath = child2.FirstChild.InnerText;
-                            
+
                             if (hintPath.Contains("Mono.Posix.dll"))
                             {
-                            	hintPath = hintPath.Replace("Mono\\Mono.Posix.dll", "GNU\\GNU.Gettext.dll").
-                            		Replace("Mono/Mono.Posix.dll", "GNU/GNU.Gettext.dll");
-                            	child2.FirstChild.InnerText = hintPath;
+                                hintPath = hintPath.Replace("Mono\\Mono.Posix.dll", "GNU\\GNU.Gettext.dll").
+                                           Replace("Mono/Mono.Posix.dll", "GNU/GNU.Gettext.dll");
+                                child2.FirstChild.InnerText = hintPath;
                             }
 
                             if (hintPath.Contains("csharp"))
@@ -323,7 +323,7 @@ public class TFixProjectReferences : TCSProjTools
                                 }
                             }
 
-                            // check if the path exists 
+                            // check if the path exists
                             // this currently does not work before any dlls have been compiled
                             if (!Directory.Exists(Path.GetDirectoryName(hintPath)))
                             {
