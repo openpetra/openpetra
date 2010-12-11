7776557ada3a3305e097f941000a2cb1d77c3f10
christiank <christian.kendel@ict.om.org>
2010-12-11 01:11:07 +0000

    *Partner Find Screen improvements* * Merge in code from Petra 2.3 for nicer layout and much improved functionality in the Partner Info Panel.

--- csharp/ICT/Petra/Client/lib/MPartner/gui/UC_PartnerFind_PartnerInfo.Designer.cs
+++ csharp/ICT/Petra/Client/lib/MPartner/gui/UC_PartnerFind_PartnerInfo.Designer.cs
@@ -1,4 +1,4 @@
-//
+﻿//
 // DO NOT REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 //
 // @Authors:
@@ -55,92 +55,109 @@ namespace Ict.Petra.Client.MPartner.Gui
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources =
+                new System.ComponentModel.ComponentResourceManager(typeof(TUC_PartnerFind_PartnerInfo));
             this.pnlPartnerDetails = new System.Windows.Forms.Panel();
-            this.ucoPartnerInfo = new Ict.Petra.Client.MPartner.Gui.TUC_PartnerInfo();
             this.pnlPartnerDetailsTitle = new System.Windows.Forms.Panel();
             this.lblDetailHeading = new System.Windows.Forms.Label();
             this.btnTogglePartnerDetails = new System.Windows.Forms.Button();
+            this.imlUpDownArrows = new System.Windows.Forms.ImageList(this.components);
+            this.ucoPartnerInfo = new Ict.Petra.Client.MPartner.Gui.TUC_PartnerInfo();
+            this.tipCollapseExpandHints = new System.Windows.Forms.ToolTip(this.components);
             this.pnlPartnerDetails.SuspendLayout();
             this.pnlPartnerDetailsTitle.SuspendLayout();
             this.SuspendLayout();
-
             //
             // pnlPartnerDetails
             //
-            this.pnlPartnerDetails.Anchor =
-                ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) |
-                                                       System.Windows.Forms.AnchorStyles.Left) |
-                                                      System.Windows.Forms.AnchorStyles.Right)));
-            this.pnlPartnerDetails.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.pnlPartnerDetails.Controls.Add(this.ucoPartnerInfo);
+            this.pnlPartnerDetails.AutoSize = true;
+            this.pnlPartnerDetails.BackColor = System.Drawing.Color.Red;
             this.pnlPartnerDetails.Controls.Add(this.pnlPartnerDetailsTitle);
+            this.pnlPartnerDetails.Controls.Add(this.ucoPartnerInfo);
+            this.pnlPartnerDetails.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlPartnerDetails.Location = new System.Drawing.Point(0, 0);
             this.pnlPartnerDetails.Name = "pnlPartnerDetails";
-            this.pnlPartnerDetails.Size = new System.Drawing.Size(495, 150);
+            this.pnlPartnerDetails.Size = new System.Drawing.Size(424, 150);
             this.pnlPartnerDetails.TabIndex = 2;
-
-            //
-            // tuC_PartnerInfo1
-            //
-            this.ucoPartnerInfo.BackColor = System.Drawing.SystemColors.Info;
-            this.ucoPartnerInfo.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ucoPartnerInfo.Font =
-                new System.Drawing.Font("Verdana", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ucoPartnerInfo.Location = new System.Drawing.Point(0, 24);
-            this.ucoPartnerInfo.MainDS = null;
-            this.ucoPartnerInfo.Margin = new System.Windows.Forms.Padding(0);
-            this.ucoPartnerInfo.Name = "tuC_PartnerInfo1";
-            this.ucoPartnerInfo.Size = new System.Drawing.Size(495, 126);
-            this.ucoPartnerInfo.TabIndex = 2;
-            this.ucoPartnerInfo.VerificationResultCollection = null;
-
             //
             // pnlPartnerDetailsTitle
             //
-            this.pnlPartnerDetailsTitle.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pnlPartnerDetailsTitle.BackColor = System.Drawing.Color.LightSteelBlue;
             this.pnlPartnerDetailsTitle.Controls.Add(this.lblDetailHeading);
             this.pnlPartnerDetailsTitle.Controls.Add(this.btnTogglePartnerDetails);
             this.pnlPartnerDetailsTitle.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlPartnerDetailsTitle.Location = new System.Drawing.Point(0, 0);
             this.pnlPartnerDetailsTitle.Name = "pnlPartnerDetailsTitle";
-            this.pnlPartnerDetailsTitle.Size = new System.Drawing.Size(495, 24);
+            this.pnlPartnerDetailsTitle.Size = new System.Drawing.Size(424, 24);
             this.pnlPartnerDetailsTitle.TabIndex = 1;
+            this.tipCollapseExpandHints.SetToolTip(this.pnlPartnerDetailsTitle, "Click here to expand / collapse the Partner Info panel");
+            this.pnlPartnerDetailsTitle.MouseLeave += new System.EventHandler(this.BtnTogglePartnerDetailsMouseLeave);
             this.pnlPartnerDetailsTitle.Click += new System.EventHandler(this.BtnTogglePartnerDetailsClick);
-
+            this.pnlPartnerDetailsTitle.MouseEnter += new System.EventHandler(this.BtnTogglePartnerDetailsMouseEnter);
             //
             // lblDetailHeading
             //
             this.lblDetailHeading.Font =
                 new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDetailHeading.ForeColor = System.Drawing.Color.Blue;
+            this.lblDetailHeading.ForeColor = System.Drawing.Color.MediumBlue;
             this.lblDetailHeading.Location = new System.Drawing.Point(2, 5);
             this.lblDetailHeading.Name = "lblDetailHeading";
-            this.lblDetailHeading.Size = new System.Drawing.Size(95, 18);
+            this.lblDetailHeading.Size = new System.Drawing.Size(144, 18);
             this.lblDetailHeading.TabIndex = 1;
             this.lblDetailHeading.Text = "Partner Info";
+            this.tipCollapseExpandHints.SetToolTip(this.lblDetailHeading, "Click here to expand / collapse the Partner Info panel");
             this.lblDetailHeading.Click += new System.EventHandler(this.BtnTogglePartnerDetailsClick);
-
             //
             // btnTogglePartnerDetails
             //
             this.btnTogglePartnerDetails.Anchor =
                 ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnTogglePartnerDetails.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.btnTogglePartnerDetails.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnTogglePartnerDetails.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnTogglePartnerDetails.FlatAppearance.BorderColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnTogglePartnerDetails.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnTogglePartnerDetails.Font = new System.Drawing.Font("Verdana",
                 7F,
                 System.Drawing.FontStyle.Bold,
                 System.Drawing.GraphicsUnit.Point,
                 ((byte)(0)));
-            this.btnTogglePartnerDetails.Location = new System.Drawing.Point(471, 2);
+            this.btnTogglePartnerDetails.ImageIndex = 0;
+            this.btnTogglePartnerDetails.ImageList = this.imlUpDownArrows;
+            this.btnTogglePartnerDetails.Location = new System.Drawing.Point(403, 2);
             this.btnTogglePartnerDetails.Name = "btnTogglePartnerDetails";
-            this.btnTogglePartnerDetails.Size = new System.Drawing.Size(19, 19);
+            this.btnTogglePartnerDetails.Size = new System.Drawing.Size(18, 18);
             this.btnTogglePartnerDetails.TabIndex = 0;
-            this.btnTogglePartnerDetails.Text = "v";
-            this.btnTogglePartnerDetails.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnTogglePartnerDetails.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.tipCollapseExpandHints.SetToolTip(this.btnTogglePartnerDetails, "Expands / collapses the Partner Info panel");
             this.btnTogglePartnerDetails.UseVisualStyleBackColor = false;
+            this.btnTogglePartnerDetails.MouseLeave += new System.EventHandler(this.BtnTogglePartnerDetailsMouseLeave);
             this.btnTogglePartnerDetails.Click += new System.EventHandler(this.BtnTogglePartnerDetailsClick);
-
+            this.btnTogglePartnerDetails.MouseEnter += new System.EventHandler(this.BtnTogglePartnerDetailsMouseEnter);
+            //
+            // imlUpDownArrows
+            //
+            this.imlUpDownArrows.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imlUpDownArrows.ImageStream")));
+            this.imlUpDownArrows.TransparentColor = System.Drawing.Color.Transparent;
+            this.imlUpDownArrows.Images.SetKeyName(0, "HCollapseDown.ico");
+            this.imlUpDownArrows.Images.SetKeyName(1, "HCollapseUp.ico");
+            this.imlUpDownArrows.Images.SetKeyName(2, "HCollapseUpHover.ico");
+            this.imlUpDownArrows.Images.SetKeyName(3, "HCollapseDownHover.ico");
+            //
+            // ucoPartnerInfo
+            //
+            this.ucoPartnerInfo.Anchor =
+                ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) |
+                                                       System.Windows.Forms.AnchorStyles.Left) |
+                                                      System.Windows.Forms.AnchorStyles.Right)));
+            this.ucoPartnerInfo.BackColor = System.Drawing.SystemColors.Info;
+            this.ucoPartnerInfo.Font = new System.Drawing.Font("Verdana", 6.75F);
+            this.ucoPartnerInfo.Location = new System.Drawing.Point(0, 24);
+            this.ucoPartnerInfo.MainDS = null;
+            this.ucoPartnerInfo.Margin = new System.Windows.Forms.Padding(0);
+            this.ucoPartnerInfo.Name = "ucoPartnerInfo";
+            this.ucoPartnerInfo.Size = new System.Drawing.Size(424, 126);
+            this.ucoPartnerInfo.TabIndex = 2;
+            this.ucoPartnerInfo.VerificationResultCollection = null;
             //
             // TUC_PartnerFind_PartnerInfo
             //
@@ -151,12 +168,15 @@ namespace Ict.Petra.Client.MPartner.Gui
             this.Controls.Add(this.pnlPartnerDetails);
             this.Margin = new System.Windows.Forms.Padding(0);
             this.Name = "TUC_PartnerFind_PartnerInfo";
-            this.Size = new System.Drawing.Size(495, 150);
+            this.Size = new System.Drawing.Size(424, 150);
             this.pnlPartnerDetails.ResumeLayout(false);
             this.pnlPartnerDetailsTitle.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
+        private System.Windows.Forms.ToolTip tipCollapseExpandHints;
+        private System.Windows.Forms.ImageList imlUpDownArrows;
         private Ict.Petra.Client.MPartner.Gui.TUC_PartnerInfo ucoPartnerInfo;
         private System.Windows.Forms.Button btnTogglePartnerDetails;
         private System.Windows.Forms.Label lblDetailHeading;
--- csharp/ICT/Petra/Client/lib/MPartner/gui/UC_PartnerFind_PartnerInfo.cs
+++ csharp/ICT/Petra/Client/lib/MPartner/gui/UC_PartnerFind_PartnerInfo.cs
@@ -25,9 +25,12 @@ using System;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Threading;
 using System.Windows.Forms;
 using GNU.Gettext;
+
 using Ict.Common;
+using Ict.Petra.Client.App.Core;
 using Ict.Petra.Client.CommonForms;
 
 namespace Ict.Petra.Client.MPartner.Gui
@@ -39,10 +42,10 @@ namespace Ict.Petra.Client.MPartner.Gui
     public partial class TUC_PartnerFind_PartnerInfo : TPetraUserControl, IPartnerInfoMethods
     {
         /// <summary>todoComment</summary>
-        public const Int16 COLLAPSEDHEIGHT = 29;
+        private const Int16 COLLAPSEDHEIGHT = 29;
 
         /// <summary>todoComment</summary>
-        public const Int16 EXPANDEDHEIGHT = 153;
+        private const Int16 EXPANDEDHEIGHT = 153;
 
         private bool FIsCollapsed = false;
 
@@ -59,12 +62,20 @@ namespace Ict.Petra.Client.MPartner.Gui
 
             // this code has been inserted by GenerateI18N, all changes in this region will be overwritten by GenerateI18N
             this.lblDetailHeading.Text = Catalog.GetString("Partner Info");
-            this.btnTogglePartnerDetails.Text = Catalog.GetString("v");
             #endregion
 
             //
             // TODO: Add constructor code after the InitializeComponent() call.
             //
+        }
+
+        /// <summary>
+        /// Stops the Timer for the fetching of data. This is needed to allow a garbage-collection of the UC_PartnerInfo UserControl
+        /// (a System.Threading.Timer is an unmanaged resource and needs manual Disposal, plus it holds a reference to the Class that instantiated it!)
+        /// </summary>
+        public void StopTimer()
+        {
+            ucoPartnerInfo.StopTimer();
         }
 
         /// <summary>todoComment</summary>
@@ -78,6 +89,45 @@ namespace Ict.Petra.Client.MPartner.Gui
             set
             {
                 FIsCollapsed = value;
+            }
+        }
+
+        /// <summary>todoComment</summary>
+        public int CollapsedHeight
+        {
+            get
+            {
+                if (TClientSettings.GUIRunningOnNonStandardDPI)
+                {
+                    return (int)(COLLAPSEDHEIGHT * ((double)this.ParentForm.AutoScaleBaseSize.Width /
+                                                    (double)PetraForm.AUTOSCALEBASESIZEWIDTHFOR96DPI));
+                }
+                else
+                {
+                    return COLLAPSEDHEIGHT;
+                }
+            }
+        }
+
+        /// <summary>todoComment</summary>
+        public int ExpandedHeight
+        {
+            get
+            {
+                int HeightOnNonStandardDPI;
+
+                if (TClientSettings.GUIRunningOnNonStandardDPI)
+                {
+                    HeightOnNonStandardDPI = (int)(EXPANDEDHEIGHT * ((double)this.ParentForm.AutoScaleBaseSize.Width /
+                                                                     (double)PetraForm.AUTOSCALEBASESIZEWIDTHFOR96DPI));
+
+                    // We only need 96% of that calculated value - otherwise it's to high (at least at 120DPI)...
+                    return (HeightOnNonStandardDPI / 100) * 96;
+                }
+                else
+                {
+                    return EXPANDEDHEIGHT;
+                }
             }
         }
 
@@ -110,8 +160,8 @@ namespace Ict.Petra.Client.MPartner.Gui
         {
             FIsCollapsed = true;
 
-            this.btnTogglePartnerDetails.Text = "^";
-            this.Height = COLLAPSEDHEIGHT;
+            btnTogglePartnerDetails.ImageIndex = 1;
+            this.Height = this.CollapsedHeight;
         }
 
         /// <summary>
@@ -121,9 +171,11 @@ namespace Ict.Petra.Client.MPartner.Gui
         {
             FIsCollapsed = false;
 
-            this.btnTogglePartnerDetails.Text = "v";
-            this.Height = EXPANDEDHEIGHT;
+            btnTogglePartnerDetails.ImageIndex = 0;
+            this.Height = this.ExpandedHeight;
         }
+
+        #region Event Handlers
 
         private void BtnTogglePartnerDetailsClick(object sender, EventArgs e)
         {
@@ -138,6 +190,40 @@ namespace Ict.Petra.Client.MPartner.Gui
                 OnCollapsed();
             }
         }
+
+        void BtnTogglePartnerDetailsMouseEnter(object sender, EventArgs e)
+        {
+//            TLogging.Log("BtnTogglePartnerDetailsMouseEnter: btnTogglePartnerDetails.ImageIndex: " + btnTogglePartnerDetails.ImageIndex.ToString());
+            if (btnTogglePartnerDetails.ImageIndex == 0)
+            {
+                btnTogglePartnerDetails.ImageIndex = 3;
+            }
+            else
+            {
+                btnTogglePartnerDetails.ImageIndex = 2;
+            }
+
+            lblDetailHeading.ForeColor = System.Drawing.Color.RoyalBlue;      // RoyalBlue;  SteelBlue     // Blue
+//            TLogging.Log("BtnTogglePartnerDetailsMouseEnter END: btnTogglePartnerDetails.ImageIndex: " + btnTogglePartnerDetails.ImageIndex.ToString());
+        }
+
+        void BtnTogglePartnerDetailsMouseLeave(object sender, EventArgs e)
+        {
+//            TLogging.Log("BtnTogglePartnerDetailsMouseLeave: btnTogglePartnerDetails.ImageIndex: " + btnTogglePartnerDetails.ImageIndex.ToString());
+            if (btnTogglePartnerDetails.ImageIndex == 3)
+            {
+                btnTogglePartnerDetails.ImageIndex = 0;
+            }
+            else if (btnTogglePartnerDetails.ImageIndex != 0)
+            {
+                btnTogglePartnerDetails.ImageIndex = 1;
+            }
+
+            lblDetailHeading.ForeColor = System.Drawing.Color.MediumBlue;  // Blue     // DarkBlue
+//            TLogging.Log("BtnTogglePartnerDetailsMouseLeave END: btnTogglePartnerDetails.ImageIndex: " + btnTogglePartnerDetails.ImageIndex.ToString());
+        }
+
+        #endregion
 
         #region Method calls that get passed through to ucoPartnerInfo
 
