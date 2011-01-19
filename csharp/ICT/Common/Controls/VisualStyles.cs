b81d5d04785b64c5f35efee46411057ed3076f6d
chadds <chadd_shaffer@taylor.edu>
2011-01-19 15:39:46 +0000

    Updated visual styles comments

--- csharp/ICT/Common/Controls/VisualStyles.cs
+++ csharp/ICT/Common/Controls/VisualStyles.cs
@@ -29,69 +29,112 @@ namespace Ict.Common.Controls
        {
                //Text Variables
                private Font InternalTitleText;
+                /// <summary>The TitleText property gets the value of InternalTitleText</summary>
+               /// </value>The TitleText Property represents the Font type of the Title Text of the user control.</value>
                public Font TitleText {get{return InternalTitleText;}}
                
                private Font InternalContentText;
+               /// <summary>The ContentText property gets the value of InternalContentText</summary>
+               /// <value>The ContentText property represents the Font specifications of the ContentText of the user control</value>               
                public Font ContentText{get{return InternalContentText;}}
 
                private Color InternalTitleTextColour;
+                /// <summary>The TitleTextxColour gets the value of the InternalTitleTextColour </summary>
+               /// <value> The TItleTextColour property represents the Colour specification of the TitleText of the user control</value>               
                public Color TitleTextColour {get{return InternalTitleTextColour;}}
 
                private Color InternalContentTextColour;
+               /// <summary>The HoverTitleTextColour property gets the value of the InternalHoverTitleTextColour</summary>
+               /// <value> The HoverTitleTextColour property represents the Colour specification of the title text when the mouse hovers over the title </value>               
                public Color ContentTextColour {get{return InternalContentTextColour;}}
 
                private Color InternalHoverTitleTextColour;
+               /// <summary>The HoverTitleTextColour property gets the value of the InternalHoverTitleTextColour</summary>
+               /// <value> The HoverTitleTextColour property represents the Colour specification of the title text when the mouse hovers over the title </value>               
                public Color HoverTitleTextColour {get{return InternalHoverTitleTextColour;}}
 
                private int InternalTitleHeight;
+               /// <summary>The TitleHeight property gets the value of the InternalTitleHeight</summary>
+               /// <value> The TitleHeight property represents the height of the TitleText</value>               
                public int TitleHeight {get{return InternalTitleHeight;}}
 
                private Color InternalContentActivatedTextColour;
+               /// <summary>The ContentActivatedTextColour property gets the value of the InternalContentActivatedTextColour</summary>
+               /// <value> The ContentActivatedTextColour property represents the colour of activated content text</value>               
                public Color ContentActivatedTextColour {get{return InternalContentActivatedTextColour;}}
 
                private Color InternalContentHoverTextColour;
+               /// <summary>The ContentHoverTextColour property gets the value of the InternalContentHoverTextColour</summary>
+               /// <value> The ContentHOverTextColour property represents the colour of the content text when the mouse hovers over it</value>               
                public Color ContentHoverTextColour {get{return InternalContentHoverTextColour;}}
                
                //Background Variables
                private Color InternalContentBackgroundColour;
+               /// <summary>The ContentBackgroundColour property gets the value of the InternalContentBackgroundColour </summary>
+               /// <value> The ContentBackgroundColour property represents the content's background color.</value>               
                public Color ContentBackgroundColour {get{return InternalContentBackgroundColour;}}
 
                private Color InternalTitleBackgroundColour;
+               /// <summary>The TitleBackgroundColour property gets the value of the IntenralTitleBackgroundColour</summary>
+               /// <value> The TItleBackgroundColour property represents the title's background colour</value>               
                public Color TitleBackgroundColour {get{return InternalTitleBackgroundColour;}}
                
                private Color InternalContentHoverBackgroundColour;
+               /// <summary>The ContentHoverBackgroundColour property gets the value of the InternalContentHoverBackgroundColour</summary>
+               /// <value> The ContentHOoverBackgroundColour property represent the content's background colour when the mouse hovers over it</value>               
                public Color ContentHoverBackgroundColour {get{return InternalContentHoverBackgroundColour;}}
                
                private Color InternalContentActivatedBackgroundColour;
+               /// <summary>The ContentActivatedBackgroundColour property gets the value of the InternalContentAcivatedBackgroundColour </summary>
+               /// <value> The ContentActivatedBackgroundCOlour property representf the content background colour when the content is activated </value>               
                public Color ContentActivatedBackgroundColour {get{return InternalContentActivatedBackgroundColour;}}
                
                //Gradient Variables
                private Color InternalPanelGradientStart;
+               /// <summary>The PanelGradientStart property gets the value of the InternalPanelGradientStart</summary>
+               /// <value> The PanelGradientStart property represents the beginning colour of the panel gradient</value>               
                public Color PanelGradientStart {get{return InternalPanelGradientStart;}}
 
                private Color InternalPanelGradientEnd;
+               /// <summary>The PanelGradientEnd property gets the value of the InternalPanelGradientEnd</summary>
+               /// <value> The PanelGradientEnd property represents the end colour of the title panel gradient.</value>               
                public Color PanelGradientEnd {get{return InternalPanelGradientEnd;}}
 
                private LinearGradientMode InternalPanelGradientMode;
+               /// <summary>The PanelGradientMode property gets the value of the InternalPanelGradientMode</summary>
+               /// <value> The PanelGradientMode property represents the direction of the gradient in the title panel gradient</value>               
                public LinearGradientMode PanelGradientMode {get{return InternalPanelGradientMode;}}
 
                private Color InternalContentGradientStart;
+               /// <summary>The ContentGradientStart property gets the value of the InternalContentGradientstart</summary>
+               /// <value> The ContentGradeientStart property represents the beginning colour of the content panel gradient</value>               
                public Color ContentGradientStart {get{return InternalContentGradientStart;}}
 
                private Color InternalContentGradientEnd;
+               /// <summary>The ContentGradientEnd property gets the value of the InternalContentGradientEnd</summary>
+               /// <value> THe ContentGradientEnd property represents the end colour of the content panel gradient</value>               
                public Color ContentGradientEnd {get{return InternalContentGradientEnd;}}
 
                private LinearGradientMode InternalContentGradientMode;
+               /// <summary>The ContentGradientMode property gets the value of the InternalContentGradientMode</summary>
+               /// <value> The ContentGradientMode property represents the direction of the gradient in the content panel gradient</value>               
                public LinearGradientMode ContentGradientMode {get{return InternalContentGradientMode;}}
 
                //Layout Variables
                private int InternalTaskIndentation;
+               /// <summary> The TaskIndentation property gets the value of the InternalTaskIndentation</summary>
+               /// <value> The TaskIndentation property represents the number of spaces the tasks are indented in the content panel</value>               
                public int TaskIndentation {get{return InternalTaskIndentation;}}
 
                private bool InternalAutomaticNumbering;
+               /// <summary>The AutomaticNumbering property gets the value of the InternalAutomaticNumbering</summary>
+               /// <value> The AutomaticNumbering property determines if automatic numbering is on</value>               
                public bool AutomaticNumbering {get{return InternalAutomaticNumbering;}}
                
                //bool variables for determining style in TaskList
+               /// <summary>
+               /// The following booleans determine which styles use what type of variables.
+               /// </summary>
                public bool UsePanelGradient;
                public bool UseContentBackgroundColours;
                public bool UseContentGradient;
@@ -101,7 +144,9 @@ namespace Ict.Common.Controls
                //Constructor
                public TVisualStyles(TVisualStylesEnum style)
                {
+               	///<summary> A switch that assigns the above variables with the specifications based on the style type</summary>
                        switch(style){
+               			      
                                case TVisualStylesEnum.vsAccordionPanel:
                                        //TitleText Variables
                                        InternalTitleText = new System.Drawing.Font("Verdana",8, System.Drawing.FontStyle.Bold);
