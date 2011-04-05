2d0001d97d3fb6427eeddbb393245b55770a7feb
Wolfgang Uhr <wolfgang.uhr@solidcharity.com>
2011-04-05 10:32:52 +0200

    GetCurrencyInfo upgrade

--- csharp/ICT/Petra/Server/lib/MFinance/GL/GL.CommonTools.cs
+++ csharp/ICT/Petra/Server/lib/MFinance/GL/GL.CommonTools.cs
@@ -24,11 +24,15 @@
 using System;
 using System.Data;
 using System.Data.Odbc;
+using System.Text.RegularExpressions;
 using Ict.Common;
 using Ict.Common.DB;
 using Ict.Common.Verification;
 using Ict.Petra.Shared.MFinance.Account.Data;
 using Ict.Petra.Server.MFinance.Account.Data.Access;
+using Ict.Petra.Shared.MCommon.Data;
+using Ict.Petra.Server.MCommon.Data.Access;
+
 
 namespace Ict.Petra.Server.MFinance.GL
 {
@@ -623,6 +627,246 @@ namespace Ict.Petra.Server.MFinance.GL
             get
             {
                 return strErrorCode;
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Base on the idea to reduce the number of database request to it's minimum, this object reads
+    /// the complete a_currency table. Two currency slots are provided, a base currency slot and a foreigen
+    /// currency slot. The the base currency slot can only set in one of the constructors one time and the
+    /// foreign currency slot can easily be switched to an other currency by using the
+    /// ForeignCurrencyCode property without and any more database request.
+    ///
+    /// The petra table a_currency contains a "hidden information" about the number of digits and this
+    /// value will be used either to calculate the number of value dependet rounding digits.
+    /// Furthermore there are some servic routines base on this information.
+    /// </summary>
+
+    public class GetCurrencyInfo
+    {
+        private ACurrencyTable currencyTable = null;
+        private ACurrencyRow baseCurrencyRow = null;
+        private ACurrencyRow foreignCurrencyRow = null;
+        private int intBaseCurrencyDigits;
+        private int intForeignCurrencyDigits;
+        private const int DIGIT_INIT_VALUE = -1;
+
+        /// <summary>
+        /// Constructor which automatically loads the table and sets the value of the
+        /// currency table.
+        /// </summary>
+        /// <param name="ACurrencyCode">Three digit description to define the
+        /// base currency.</param>
+        public GetCurrencyInfo(string ACurrencyCode)
+        {
+            LoadDatabase();
+            baseCurrencyRow = SetRowToCode(ACurrencyCode);
+        }
+
+        /// <summary>
+        /// Constructor which automatically loads the table and sets the value of
+        /// the base currency table and foreign currency table.
+        /// </summary>
+        /// <param name="ABaseCurrencyCode">Base currency code</param>
+        /// <param name="AForeignCurrencyCode">foreign Currency Code</param>
+        public GetCurrencyInfo(string ABaseCurrencyCode, string AForeignCurrencyCode)
+        {
+            LoadDatabase();
+            baseCurrencyRow = SetRowToCode(ABaseCurrencyCode);
+            foreignCurrencyRow = SetRowToCode(AForeignCurrencyCode);
+        }
+
+        private void LoadDatabase()
+        {
+            intBaseCurrencyDigits = DIGIT_INIT_VALUE;
+            intForeignCurrencyDigits = DIGIT_INIT_VALUE;
+            TDBTransaction transaction = DBAccess.GDBAccessObj.BeginTransaction();
+            currencyTable = ACurrencyAccess.LoadAll(transaction);
+            DBAccess.GDBAccessObj.CommitTransaction();
+
+            if (currencyTable.Rows.Count == 0)
+            {
+                throw new InternalException("GetCurrencyInfo.01",
+                    Catalog.GetString(
+                        "The table a_currency is empty!"));
+            }
+        }
+
+        private ACurrencyRow SetRowToCode(string ACurrencyCode)
+        {
+            if (ACurrencyCode.Equals(String.Empty))
+            {
+                return null;
+            }
+
+            for (int i = 0; i < currencyTable.Rows.Count; ++i)
+            {
+                if (ACurrencyCode.Equals(((ACurrencyRow)currencyTable[i]).CurrencyCode))
+                {
+                    return (ACurrencyRow)currencyTable[i];
+                }
+            }
+
+            throw new InternalException("GetCurrencyInfo.02",
+                Catalog.GetString(String.Format(
+                        "No Data for curency {0} found", ACurrencyCode)));
+        }
+
+        /// <summary>
+        /// Property to read the base currency code value.
+        /// </summary>
+        public string CurrencyCode
+        {
+            get
+            {
+                return baseCurrencyRow.CurrencyCode;
+            }
+        }
+
+        /// <summary>
+        /// Property to read and to set the foreign currency code value.
+        /// </summary>
+        public string ForeignCurrencyCode
+        {
+            get
+            {
+                return foreignCurrencyRow.CurrencyCode;
+            }
+            set
+            {
+                intForeignCurrencyDigits = DIGIT_INIT_VALUE;
+                foreignCurrencyRow = SetRowToCode(value);
+            }
+        }
+
+        /// <summary>
+        /// This rotine handles a correct roundig by using the number of digits in a_currency
+        /// </summary>
+        /// <param name="AValueToRound"></param>
+        /// <returns></returns>
+        public decimal RoundBaseCurrencyValue(decimal AValueToRound)
+        {
+            return Math.Round(AValueToRound, digits);
+        }
+
+        /// <summary>
+        /// This rotine handles a correct roundig by using the number of digits in a_currency
+        /// </summary>
+        /// <param name="AValueToRound"></param>
+        /// <returns></returns>
+        public decimal RoundForeignCurrencyValue(decimal AValueToRound)
+        {
+            return Math.Round(AValueToRound, foreignDigits);
+        }
+
+        /// <summary>
+        /// Here you can calculate the base value amount by defining the parameters below.
+        /// Because of the rounding the ToBaseValue(ToForeignValue(someValue)) != someValue
+        /// </summary>
+        /// <param name="AForeignValue"></param>
+        /// <param name="AExchangeRate"></param>
+        /// <returns></returns>
+        public decimal ToBaseValue(decimal AForeignValue, decimal AExchangeRate)
+        {
+            return RoundBaseCurrencyValue(AForeignValue * AExchangeRate);
+        }
+
+        /// <summary>
+        /// Here you can calculate the base value amount by defining the parameters below.
+        /// Because of the rounding the ToBaseValue(ToForeignValue(someValue)) != someValue
+        /// </summary>
+        /// <param name="ABaseValue"></param>
+        /// <param name="AExchangeRate"></param>
+        /// <returns></returns>
+        public decimal ToForeignValue(decimal ABaseValue, decimal AExchangeRate)
+        {
+            return RoundForeignCurrencyValue(ABaseValue / AExchangeRate);
+        }
+
+        /// <summary>
+        /// Calculates the number of digits by reading the row.DisplayFormat
+        /// Entry of the currency table and convert the old petra string to an
+        /// integer response.
+        /// </summary>
+        public int digits
+        {
+            get
+            {
+                if (intBaseCurrencyDigits == DIGIT_INIT_VALUE)
+                {
+                    intBaseCurrencyDigits = new FormatConverter(baseCurrencyRow.DisplayFormat).digits;
+                }
+
+                return intBaseCurrencyDigits;
+            }
+        }
+
+        /// <summary>
+        /// Calculates the number of digits for the foreign currency.
+        /// </summary>
+        public int foreignDigits
+        {
+            get
+            {
+                if (intForeignCurrencyDigits == DIGIT_INIT_VALUE)
+                {
+                    intForeignCurrencyDigits = new FormatConverter(foreignCurrencyRow.DisplayFormat).digits;
+                }
+
+                return intForeignCurrencyDigits;
+            }
+        }
+    }
+
+    /// <summary>
+    /// This class is a local Format converter <br />
+    ///  Console.WriteLine(new FormatConverter("->>>,>>>,>>>,>>9.99").digits.ToString());<br />
+    ///  Console.WriteLine(new FormatConverter("->>>,>>>,>>>,>>9.9").digits.ToString());<br />
+    ///  Console.WriteLine(new FormatConverter("->>>,>>>,>>>,>>9").digits.ToString());<br />
+    /// The result is 2,1 and 0 digits ..
+    /// </summary>
+    public class FormatConverter
+    {
+        string sRegex;
+        Regex reg;
+        MatchCollection matchCollection;
+        int intDigits;
+        public FormatConverter(string strFormat)
+        {
+            sRegex = ">9.(9)+|>9$";
+            reg = new Regex(sRegex);
+            matchCollection = reg.Matches(strFormat);
+
+            if (matchCollection.Count != 1)
+            {
+                throw new InternalException("GetCurrencyInfo.03",
+                    String.Format("The regular expression {0} does not fit for a match in {1}",
+                        sRegex, strFormat));
+            }
+
+            intDigits = (matchCollection[0].Value).Length - 3;
+
+            if (intDigits == -1)
+            {
+                intDigits = 0;
+            }
+
+            if (intDigits < -1)
+            {
+                intDigits = 2;
+            }
+        }
+
+        /// <summary>
+        /// Property to report the number of digits
+        /// </summary>
+        public int digits
+        {
+            get
+            {
+                return intDigits;
             }
         }
     }
--- csharp/ICT/Testing/lib/MFinance/GL-Test/Test.GL.CommonTools.cs
+++ csharp/ICT/Testing/lib/MFinance/GL-Test/Test.GL.CommonTools.cs
@@ -100,6 +100,109 @@ namespace Ict.Testing.Petra.Server.MFinance.GL
                 "This request shall fail (period does not exist)");
         }
 
+        /// <summary>
+        /// Test for the internal format converter.
+        /// </summary>
+        [Test]
+        public void Test_04_FormatConverter()
+        {
+            Assert.AreEqual(2, new FormatConverter(",>>>,>>9.99").digits, "Number of digits: 2");
+            Assert.AreEqual(1, new FormatConverter(",>>>,>>9.9").digits, "Number of digits: 1");
+            Assert.AreEqual(0, new FormatConverter(",>>>,>>9").digits, "Number of digits: 0");
+            try
+            {
+                decimal d = new FormatConverter("nonsens").digits;
+                Assert.Fail("No InternalException thrown");
+            }
+            catch (InternalException internalException)
+            {
+                Assert.AreEqual("GetCurrencyInfo.03", internalException.ErrorCode, "Wrong Error Code");
+            }
+            catch (Exception)
+            {
+                Assert.Fail("Other than InternalException thrown");
+            }
+        }
+
+        /// <summary>
+        /// Test of the internal routine GetCurrencyInfo
+        /// </summary>
+        [Test]
+        public void Test_05_GetCurrencyInfo()
+        {
+            Assert.AreEqual(2, new GetCurrencyInfo("EUR").digits, "Number of digits: 2");
+            try
+            {
+                decimal d = new GetCurrencyInfo("JPN").digits;
+                Assert.Fail("No InternalException thrown");
+            }
+            catch (InternalException internalException)
+            {
+                Assert.AreEqual("GetCurrencyInfo.02", internalException.ErrorCode, "Wrong Error Code");
+            }
+            catch (Exception)
+            {
+                Assert.Fail("Other than InternalException thrown");
+            }
+
+            try
+            {
+                decimal d = new GetCurrencyInfo("DMG").digits;
+                Assert.Fail("No InternalException thrown");
+            }
+            catch (InternalException internalException)
+            {
+                if (internalException.ErrorCode.Equals("GetCurrencyInfo.01"))
+                {
+                    Assert.Fail("Test Data are not loaded correctly");
+                }
+                else if (internalException.ErrorCode.Equals("GetCurrencyInfo.02"))
+                {
+                    Assert.Pass("DMG-Test ok");
+                }
+                else
+                {
+                    Assert.AreEqual("GetCurrencyInfo.02",
+                        internalException.ErrorCode,
+                        "Wrong Error Code");
+                }
+            }
+            catch (Exception)
+            {
+                Assert.Fail("Other than InternalException thrown");
+            }
+        }
+
+        /// <summary>
+        /// CurrencyInfo supports two currencies and the conversion rules incluing the
+        /// rouding based on
+        /// </summary>
+        [Test]
+        public void Test_05_GetCurrencyInfo_2()
+        {
+            GetCurrencyInfo getCurrencyInfo = new GetCurrencyInfo("EUR", "JPY");
+
+            Assert.AreEqual(1.23m, getCurrencyInfo.RoundBaseCurrencyValue(1.23456m), "Round to 2 digits");
+            Assert.AreEqual(1.0m, getCurrencyInfo.RoundForeignCurrencyValue(1.23456m), "Round to 0 digits");
+
+            decimal exchangeRate = 1 / 119.7295m;
+
+            Assert.AreEqual(0.84m, getCurrencyInfo.ToBaseValue(100.00m, exchangeRate),
+                "Conversion from 100 YEN to 0.83 EUR");
+            Assert.AreEqual(11973, getCurrencyInfo.ToForeignValue(100.00m, exchangeRate),
+                "Conversion from 100 EUR to 11983 YEN");
+            Assert.AreEqual(120, getCurrencyInfo.ToForeignValue(1.00m, exchangeRate),
+                "Conversion from 1 EUR to 120 YEN");
+
+            getCurrencyInfo.ForeignCurrencyCode = "GBP";     // Change foreign Currency to Pound ...
+            exchangeRate = 1 / 0.8801m;
+
+            Assert.AreEqual(113.62m, getCurrencyInfo.ToBaseValue(100.00m, exchangeRate),
+                "Conversion from 100 GBP to 113.62 EUR");
+            Assert.AreEqual(88.01m, getCurrencyInfo.ToForeignValue(100.00m, exchangeRate),
+                "Conversion from 100 EUR to 88.01 GBP");
+        }
+
         private bool TryGetAccountPeriodInfo(int ALedgerNum, int APeriodNum)
         {
             try
