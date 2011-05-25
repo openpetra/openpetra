3e5e3d1ee78e93bd4bfbe510afca3dac8071f4b0
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2011-05-25 16:12:32 +0200

    fix previous commit

--- csharp/ICT/Testing/lib/MPartner/server/PartnerExports/PartnerExtracts.cs
+++ csharp/ICT/Testing/lib/MPartner/server/PartnerExports/PartnerExtracts.cs
@@ -68,8 +68,8 @@ namespace Tests.MPartner.Server.PartnerExports
         [Test]
         public void TestImportFamily()
         {
-            const string testFile = TAppSettingsManager.GetValue("ExtractTest.file", "../../csharp/ICT/Testing/lib/MPartner/SampleData/sampleExtract.ext");
-            const string SelectedEventCode = TAppSettingsManager.GetValue("ImportPartnerForEventCode", string.empty);
+            string testFile = TAppSettingsManager.GetValue("ExtractTest.file", "../../csharp/ICT/Testing/lib/MPartner/SampleData/sampleExtract.ext");
+            string SelectedEventCode = TAppSettingsManager.GetValue("ImportPartnerForEventCode", String.Empty);
             StreamReader reader = new StreamReader(testFile);
 
             string[] lines = reader.ReadToEnd().Replace("\r\n", "\n").Replace("\r", "\n").Split(new char[] { '\n' });
