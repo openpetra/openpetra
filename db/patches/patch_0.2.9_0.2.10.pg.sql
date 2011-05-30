9de047ed76b544e045e97f85488ba693f0f9973f
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2011-05-30 08:11:46 +0200

    forgot a few lines for upgrade to 0.2.10 of postgresql database

--- db/patches/patch_0.2.9_0.2.10.pg.sql
+++ db/patches/patch_0.2.9_0.2.10.pg.sql
@@ -17,3 +17,15 @@ ALTER TABLE pc_attendee RENAME COLUMN pc_xyz_tbd_type_c TO pc_outreach_type_c;
 
 ALTER TABLE pc_supplement RENAME COLUMN pc_xyz_tbd_type_c TO pc_outreach_type_c;
 
+CREATE SEQUENCE "seq_login_process_id"
+  INCREMENT BY 1
+  MINVALUE 0
+  MAXVALUE 999999999
+  START WITH 2000;
+GRANT SELECT,UPDATE,USAGE ON seq_login_process_id TO petraserver;
+
+alter table s_login drop constraint s_login_pk;
+alter table s_login add constraint s_login_uk UNIQUE (s_user_id_c, s_login_date_d, s_login_time_i);
+alter table s_login add primary key (s_login_process_id_r); 
+
+update s_system_defaults set s_default_value_c = '0.2.10-0' where s_default_code_c = 'CurrentDatabaseVersion';
