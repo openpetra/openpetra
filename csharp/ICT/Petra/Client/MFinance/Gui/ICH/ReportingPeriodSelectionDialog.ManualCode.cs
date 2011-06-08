59c6fa651b0719330802a279beb4b35e382e4c4e
christiank <christian.kendel@ict.om.org>
2011-06-08 16:10:22 +0100

    Humble beginnings of a Dialog Form for the selection of a Reporting Period. This Dialog will later launch one of two ICH programs.

---
+++ csharp/ICT/Petra/Client/MFinance/Gui/ICH/ReportingPeriodSelectionDialog.ManualCode.cs
@@ -0,0 +1,52 @@
+﻿//
+// DO NOT REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+//
+// @Authors:
+//       timop
+//
+// Copyright 2004-2011 by OM International
+//
+// This file is part of OpenPetra.org.
+//
+// OpenPetra.org is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenPetra.org is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenPetra.org.  If not, see <http://www.gnu.org/licenses/>.
+//
+using System;
+using System.Data;
+using System.Globalization;
+using System.Windows.Forms;
+using System.Threading;
+using GNU.Gettext;
+using Ict.Common;
+using Ict.Petra.Client.App.Core;
+using Ict.Petra.Client.App.Core.RemoteObjects;
+using Ict.Petra.Shared;
+using Ict.Petra.Shared.Interfaces;
+
+namespace Ict.Petra.Client.MFinance.Gui.ICH
+{    
+    /// manual methods for the generated window
+    public partial class TFrmReportingPeriodSelectionDialog
+    {
+        private void InitializeManualCode()            
+        {
+
+        }
+
+        private void BtnOK_Click(Object Sender, EventArgs e)
+        {
+
+            Close();
+        }
+    }
+}
