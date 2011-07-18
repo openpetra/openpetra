d553082bb061fbc8ce37764f33910b1dd0449b97
Chris Thomas <chris.thomas@om.org>
2011-07-18 10:07:29 +0100

    ICH Dialog - Phase 1

--- csharp/ICT/Petra/Client/MFinance/Gui/ICH/ReportingPeriodSelectionDialog.ManualCode.cs
+++ csharp/ICT/Petra/Client/MFinance/Gui/ICH/ReportingPeriodSelectionDialog.ManualCode.cs
@@ -32,21 +32,120 @@ using Ict.Petra.Client.App.Core;
 using Ict.Petra.Client.App.Core.RemoteObjects;
 using Ict.Petra.Shared;
 using Ict.Petra.Shared.Interfaces;
+using Ict.Petra.Client.CommonControls;
+using Ict.Common.Controls;
+using Ict.Petra.Shared.MFinance;
+using Ict.Petra.Shared.MFinance.Account.Data;
+
 
 namespace Ict.Petra.Client.MFinance.Gui.ICH
-{    
-    /// manual methods for the generated window
-    public partial class TFrmReportingPeriodSelectionDialog
+{   
+    /// <summary>
+    /// Enums holding the possible reporting period selection modes
+    /// </summary>
+    public enum TICHReportingPeriodSelectionModeEnum
     {
-        private void InitializeManualCode()            
+        /// <summary>
+        /// ICH Statement reporting period selection mode
+        /// </summary>
+        rpsmICHStatement,
+        /// <summary>
+        /// ICH Stewardship Calculation reporting period selection mode
+        /// </summary>
+        rpsmICHStewardshipCalc
+    }
+    
+	
+	/// manual methods for the generated window
+    public partial class TFrmReportingPeriodSelectionDialog : System.Windows.Forms.Form
+    {
+        /// <summary>
+        /// Field to store the reporting period selection mode
+        /// </summary>
+        public TICHReportingPeriodSelectionModeEnum FReportingPeriodSelectionMode = TICHReportingPeriodSelectionModeEnum.rpsmICHStewardshipCalc;
+        /// <summary>
+        /// Field to store the relevant Ledger number
+        /// </summary>
+        public Int32 FLedgerNumber = 0;
+        private int FNumberAccountingPeriods;
+        private int FNumberForwardingPeriods;
+        private int FCurrentPeriod;
+        private int FCurrentYear;
+
+        
+        private void InitializeManualCode()
         {
 
         }
 
         private void BtnOK_Click(Object Sender, EventArgs e)
         {
-
-            Close();
+           Close();
         }
+        
+        /// <summary>
+        /// Gets or sets the ICH reporting period selection mode
+        /// </summary>
+        public TICHReportingPeriodSelectionModeEnum ReportingPeriodSelectionMode
+        {
+            get
+            {
+                 return FReportingPeriodSelectionMode;     
+            }
+            
+            set
+            {
+                FReportingPeriodSelectionMode = value;
+                if (FReportingPeriodSelectionMode == TICHReportingPeriodSelectionModeEnum.rpsmICHStatement)
+                {
+                   chkEmailReport.Visible = true;
+                   lblEmailReport.Visible = true;
+                }
+                else
+                {
+                    chkEmailReport.Visible = false;
+                    lblEmailReport.Visible = false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Write-only Ledger number property
+        /// </summary>
+        public Int32 LedgerNumber
+        {
+            set
+            {
+                
+                FLedgerNumber=value;
+                string  currentPeriodDBName; 
+                 
+                DataTable Table = TDataCache.TMFinance.GetCacheableFinanceTable(TCacheableFinanceTablesEnum.AccountingPeriodList, FLedgerNumber);
+
+                //Ict.Petra.Client.CommonControls.TCmbAutoPopulated this.Controls["cmbReportPeriod"];
+                TCmbAutoPopulated cmb = cmbReportPeriod;
+                
+                TRemote.MFinance.Reporting.UIConnectors.SelectLedger(FLedgerNumber);
+                TRemote.MFinance.Reporting.UIConnectors.GetLedgerPeriodDetails(out FNumberAccountingPeriods,
+                                                                               out FNumberForwardingPeriods,
+                                                                               out FCurrentPeriod,
+                                                                               out FCurrentYear);
+
+                Table.DefaultView.Sort = AAccountingPeriodTable.GetAccountingPeriodDescDBName() + " ASC";
+                
+                currentPeriodDBName = AAccountingPeriodTable.GetAccountingPeriodNumberDBName();
+                Table.DefaultView.RowFilter = currentPeriodDBName + " >= " + FCurrentPeriod +
+                                              " AND " + 
+                                              currentPeriodDBName + " <= " + FNumberForwardingPeriods + FCurrentPeriod + "";
+                                              //" AND " + AAccountingPeriodTable. ;
+                
+                cmb.InitialiseUserControl(Table, AAccountingPeriodTable.GetAccountingPeriodNumberDBName(), AAccountingPeriodTable.GetAccountingPeriodDescDBName(), null, null);
+    
+                cmb.AppearanceSetup(new int[] { -1 }, -1);
+
+            
+            }
+        }
+
     }
 }
