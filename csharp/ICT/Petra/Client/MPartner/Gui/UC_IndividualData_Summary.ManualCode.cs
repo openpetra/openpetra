c5db15f87506e498f0c6cc3908d72a916cbc1df4
christiank <christiank@om.org>
2011-10-19 16:40:30 +0100

    Remove separate logic c# file - this is the way we did it in Petra 2.3, but in OpenPetra we have the *.ManualCode.cs files for that...

--- csharp/ICT/Petra/Client/MPartner/Gui/UC_IndividualData_Summary.ManualCode.cs
+++ csharp/ICT/Petra/Client/MPartner/Gui/UC_IndividualData_Summary.ManualCode.cs
@@ -22,13 +22,16 @@
 // along with OpenPetra.org.  If not, see <http://www.gnu.org/licenses/>.
 //
 using System;
+using System.Data;
 using System.Windows.Forms;
 
 using Ict.Common;
+using Ict.Petra.Client.App.Core;
 using Ict.Petra.Client.MPartner;
 using Ict.Petra.Shared.Interfaces.MPartner.Partner.UIConnectors;
 using Ict.Petra.Shared.MPartner.Partner.Data;
 using Ict.Petra.Shared.MPersonnel.Personnel.Data;
+using Ict.Petra.Shared.MPersonnel.Person;
 
 namespace Ict.Petra.Client.MPartner.Gui
 {        
@@ -36,8 +39,6 @@ namespace Ict.Petra.Client.MPartner.Gui
     {   
         /// <summary>holds a reference to the Proxy System.Object of the Serverside UIConnector</summary>
         private IPartnerUIConnectorsPartnerEdit FPartnerEditUIConnector;
-        
-        private TUC_IndividualData_SummaryLogic FLogic;
         
         #region Properties
         
@@ -64,13 +65,7 @@ namespace Ict.Petra.Client.MPartner.Gui
         {
             FMainDS = AMainDS;
         
-            // Define the screen's logic
-            FLogic = new TUC_IndividualData_SummaryLogic();
-            
-            // Set up screen logic
-            FLogic.MainDS = FMainDS;
-            FLogic.PartnerEditUIConnector = FPartnerEditUIConnector;
-            FLogic.LoadDataOnDemand();
+			LoadDataOnDemand();
             
             ShowData((PPersonRow)FMainDS.PPerson.Rows[0]);  
             
@@ -96,5 +91,58 @@ namespace Ict.Petra.Client.MPartner.Gui
         {
             
         }
+        
+        /// <summary>
+        /// Loads Summary Data from Petra Server into FMainDS, if not already loaded.
+        /// </summary>
+        /// <returns>true if successful, otherwise false.</returns>
+        private Boolean LoadDataOnDemand()
+        {
+            Boolean ReturnValue;
+
+            try
+            {
+                // Make sure that Typed DataTables are already there at Client side
+                if (FMainDS.SummaryData == null)
+                {
+                    FMainDS.Tables.Add(new IndividualDataTDSSummaryDataTable());
+                    FMainDS.InitVars();
+                }
+
+                if (TClientSettings.DelayedDataLoading && 
+                    (FMainDS.SummaryData.Rows.Count == 0))
+                {
+                    FMainDS.Merge(FPartnerEditUIConnector.GetDataPersonnelIndividualData(TIndividualDataItemEnum.idiSummary));
+
+                    // Make DataRows unchanged
+                    if (FMainDS.SummaryData.Rows.Count > 0)
+                    {
+                        if (FMainDS.SummaryData.Rows[0].RowState != DataRowState.Added)
+                        {
+                            FMainDS.SummaryData.AcceptChanges();
+                        }
+                    }
+                }
+
+                if (FMainDS.SummaryData.Rows.Count != 0)
+                {
+                    ReturnValue = true;
+                }
+                else
+                {
+                    ReturnValue = false;
+                }
+            }
+            catch (System.NullReferenceException)
+            {
+                return false;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
+            return ReturnValue;
+        }        
     }
 }
