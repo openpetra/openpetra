338850bf602c891b5fee47104d69acb8a8f294b3
christiank <christian.kendel@om.org>
2011-11-24 15:09:05 +0000

    Added ability to call Method 'RunOnceOnActivationManual' in *.ManualCode.cs files to several code templates.

--- inc/template/src/Winforms/windowEdit.cs
+++ inc/template/src/Winforms/windowEdit.cs
@@ -180,6 +180,7 @@ namespace {#NAMESPACE}
     /// auto generated
     public void RunOnceOnActivation()
     {
+        {#RUNONCEONACTIVATIONMANUAL}
         {#RUNONCEINTERFACEIMPLEMENTATION}
     }
 
