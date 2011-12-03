2d544281de905cf2290c25cdf0746e619122266b
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2011-12-03 18:34:57 +0100

    replace Ext.reg with Ext.ComponentMgr.registerType

--- webserver/js/FileUploadField.js
+++ webserver/js/FileUploadField.js
@@ -176,7 +176,7 @@ Ext.ux.form.FileUploadField = Ext.extend(Ext.form.TextField,  {
 
 });
 
-Ext.reg('fileuploadfield', Ext.ux.form.FileUploadField);
+Ext.ComponentMgr.registerType('fileuploadfield', Ext.ux.form.FileUploadField);
 
 // backwards compat
 Ext.form.FileUploadField = Ext.ux.form.FileUploadField;
