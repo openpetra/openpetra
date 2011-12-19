be12ee00ae8f61f0d73a837a755b2a12bc1d63cd
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2011-12-19 14:22:08 +0100

    exception if control is used several times on a form

--- csharp/ICT/PetraTools/GenerateExtJsForms/ControlGeneratorBase.cs
+++ csharp/ICT/PetraTools/GenerateExtJsForms/ControlGeneratorBase.cs
@@ -161,11 +161,20 @@ namespace Ict.Tools.CodeGeneration.ExtJs
         {
         }
 
+        private StringCollection FAlreadyInserted = new StringCollection();
+
         /// <summary>
         /// fill in the attributes for the control
         /// </summary>
         public virtual ProcessTemplate SetControlProperties(TFormWriter writer, TControlDef ACtrl)
         {
+            if (FAlreadyInserted.Contains(ACtrl.controlName))
+            {
+                throw new Exception("Cannot insert control " + ACtrl.controlName + " several times into a form");
+            }
+
+            FAlreadyInserted.Add(ACtrl.controlName);
+
             ProcessTemplate snippetControl = writer.FTemplate.GetSnippet(FControlDefinitionSnippetName);
 
             snippetControl.SetCodelet("ITEMNAME", ACtrl.controlName);
