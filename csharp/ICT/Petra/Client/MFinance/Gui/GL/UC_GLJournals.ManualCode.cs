02853e868d8501cd96e792ed21adfc3767ac5fe3
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2012-03-14 18:48:30 +0100

    uncrustify

--- csharp/ICT/Petra/Client/MFinance/Gui/GL/UC_GLJournals.ManualCode.cs
+++ csharp/ICT/Petra/Client/MFinance/Gui/GL/UC_GLJournals.ManualCode.cs
@@ -156,7 +156,7 @@ namespace Ict.Petra.Client.MFinance.Gui.GL
             }
 
             dtpDetailDateEffective.AllowVerification = !FPetraUtilsObject.DetailProtectedMode;
-            
+
             UpdateChangeableStatus();
         }
 
