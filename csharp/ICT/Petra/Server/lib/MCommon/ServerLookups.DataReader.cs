4f66f8f5b2ce97731375457a7b55277bd4a51e12
christiank <christian.kendel@om.org>
2012-06-14 17:12:14 +0200

    Prevent accidental nesting of DB Transactions: TCommonDataReader.GetData now uses GetNewOrExistingTransaction instead of BeginTransaction.
    Nesting was caused by using that Method while data was being saved AND validated on server side - a DB Transaction got started by the Method that wanted to save the data which then called the data validation Method, which in turn used GetData...

--- csharp/ICT/Petra/Server/lib/MCommon/ServerLookups.DataReader.cs
+++ csharp/ICT/Petra/Server/lib/MCommon/ServerLookups.DataReader.cs
@@ -65,14 +65,17 @@ namespace Ict.Petra.Server.MCommon.DataReader
         public static bool GetData(string ATablename, TSearchCriteria[] ASearchCriteria, out TTypedDataTable AResultTable)
         {
             // TODO: check access permissions for the current user
-
+            
+            bool NewTransaction = false;
             TDBTransaction ReadTransaction;
 
             TTypedDataTable tempTable = null;
 
             try
             {
-                ReadTransaction = DBAccess.GDBAccessObj.BeginTransaction(IsolationLevel.RepeatableRead, 5);
+                ReadTransaction = DBAccess.GDBAccessObj.GetNewOrExistingTransaction(IsolationLevel.RepeatableRead, 
+                        TEnforceIsolationLevel.eilMinimum,
+                        out NewTransaction);                                                                                   
 
                 // TODO: auto generate
                 if (ATablename == AApSupplierTable.GetTableDBName())
@@ -164,19 +167,28 @@ namespace Ict.Petra.Server.MCommon.DataReader
                 }
                 else
                 {
-                    throw new Exception("TCommonDataReader.LoadData: unknown table " + ATablename);
+                    throw new Exception("TCommonDataReader.GetData: unknown table " + ATablename);
                 }
             }
             catch (Exception Exp)
             {
                 DBAccess.GDBAccessObj.RollbackTransaction();
-                TLogging.Log("TCommonDataReader.LoadData exception: " + Exp.ToString(), TLoggingType.ToLogfile);
+                TLogging.Log("TCommonDataReader.GetData exception: " + Exp.ToString(), TLoggingType.ToLogfile);
                 TLogging.Log(Exp.StackTrace, TLoggingType.ToLogfile);
                 throw Exp;
             }
             finally
             {
-                DBAccess.GDBAccessObj.CommitTransaction();
+                if (NewTransaction)
+                {
+                    DBAccess.GDBAccessObj.CommitTransaction();
+#if DEBUGMODE
+                    if (TLogging.DL >= 7)
+                    {
+                        Console.WriteLine("TCommonDataReader.GetData: committed own transaction.");
+                    }
+#endif
+                }
             }
 
             // Accept row changes here so that the Client gets 'unmodified' rows
