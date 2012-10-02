8d164fa4ef0d6403796662b56717f0060069b294
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2012-10-02 16:56:58 +0200

    add a test case for the events of the txtPetraDate control. we have the suspicion that DateChanged is not triggered when the text is changed

---
+++ csharp/ICT/Testing/lib/Common/Controls/txtPetraDate.cs
@@ -0,0 +1,87 @@
+﻿//
+// DO NOT REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+//
+// @Authors:
+//       timop
+//
+// Copyright 2004-2012 by OM International
+//
+// This file is part of OpenPetra.org.
+//
+// OpenPetra.org is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenPetra.org is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenPetra.org.  If not, see <http://www.gnu.org/licenses/>.
+//
+using System;
+using System.Data;
+using System.Configuration;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using System.IO;
+using System.Threading;
+using System.Globalization;
+using System.Windows.Forms;
+using Ict.Common.Controls;
+using Ict.Petra.Client.CommonControls;
+using Ict.Common;
+using Ict.Testing.NUnitForms;
+
+namespace Tests.Common.Controls
+{
+    /// Testing the text edit control for dates
+    [TestFixture]
+    public class TTestPetraDate
+    {
+        int DateChangedCalled = 0;
+        int TextChangedCalled = 0;
+
+        private void DateChanged(object sender, TPetraDateChangedEventArgs e)
+        {
+            TLogging.Log("DateChanged");
+            DateChangedCalled++;
+        }
+
+        private void TextChanged(object sender, EventArgs e)
+        {
+            TLogging.Log("TextChanged");
+            TextChangedCalled++;
+        }
+
+        /// <summary>
+        /// Testing the text edit control for dates
+        /// </summary>
+        [Test]
+        public void TestEvents()
+        {
+            TtxtPetraDate dtpDate = new TtxtPetraDate();
+
+            dtpDate.Name = "dtpDate";
+            dtpDate.DateChanged += new TPetraDateChangedEventHandler(DateChanged);
+            dtpDate.TextChanged += new EventHandler(TextChanged);
+
+            Form TestForm = new Form();
+            TestForm.Controls.Add(dtpDate);
+
+            TestForm.Show();
+
+            TTxtPetraDateTester tester = new TTxtPetraDateTester("dtpDate");
+
+            TextChangedCalled = 0;
+            DateChangedCalled = 0;
+            tester.Properties.Text = "31-12-2012";
+
+            Assert.AreEqual(new DateTime(2012, 12, 31), tester.Properties.Date.Value, "date should be set");
+            Assert.AreEqual(2, TextChangedCalled, "event TextChanged should have been called twice");
+            Assert.AreEqual(1, DateChangedCalled, "event DateChanged should have been called once");
+        }
+    }
+}
---
+++ csharp/ICT/Testing/lib/NUnitForms/txtPetraDateTester.cs
@@ -0,0 +1,71 @@
+﻿//
+// DO NOT REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+//
+// @Authors:
+//       timop
+//
+// Copyright 2004-2012 by OM International
+//
+// This file is part of OpenPetra.org.
+//
+// OpenPetra.org is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenPetra.org is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenPetra.org.  If not, see <http://www.gnu.org/licenses/>.
+using Ict.Common.Controls;
+using Ict.Petra.Client.CommonControls;
+using System.Windows.Forms;
+using NUnit.Extensions.Forms;
+
+namespace Ict.Testing.NUnitForms
+{
+    /// <summary>
+    /// test for our own date input textbox
+    /// </summary>
+    public class TTxtPetraDateTester : ControlTester <TtxtPetraDate, TTxtPetraDateTester>
+    {
+        /// constructor
+        public TTxtPetraDateTester()
+        {
+        }
+
+        /// constructor
+        public TTxtPetraDateTester(string name, Form form) : base(name, form)
+        {
+        }
+
+        /// constructor
+        public TTxtPetraDateTester(string name, string formName) : base(name, formName)
+        {
+        }
+
+        /// constructor
+        public TTxtPetraDateTester(string name) : base(name)
+        {
+        }
+
+        /// constructor
+        public TTxtPetraDateTester(TTxtPetraDateTester tester, int index) : base(tester, index)
+        {
+        }
+
+        /// <summary>
+        /// access the properties of the date textbox
+        /// </summary>
+        public new TtxtPetraDate Properties
+        {
+            get
+            {
+                return (TtxtPetraDate) base.TheObject;
+            }
+        }
+    }
+}
