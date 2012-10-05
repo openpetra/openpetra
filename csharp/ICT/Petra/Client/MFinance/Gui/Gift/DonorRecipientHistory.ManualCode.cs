cc34d19722fb41edefc022f49d624d961edcba0e
Tim Ingham <tim.ingham@om.org>
2012-10-05 11:33:56 +0100

    0001014: Donor and Recipient history: exception raised when trying to view the gift

--- csharp/ICT/Petra/Client/MFinance/Gui/Gift/DonorRecipientHistory.ManualCode.cs
+++ csharp/ICT/Petra/Client/MFinance/Gui/Gift/DonorRecipientHistory.ManualCode.cs
@@ -31,6 +31,7 @@ using Ict.Common.Verification;
 using Ict.Petra.Client.App.Core.RemoteObjects;
 using Ict.Petra.Shared;
 using Ict.Petra.Shared.MFinance.Gift.Data;
+using Ict.Petra.Shared.MFinance;
 
 namespace Ict.Petra.Client.MFinance.Gui.Gift
 {
@@ -195,8 +196,16 @@ namespace Ict.Petra.Client.MFinance.Gui.Gift
                     TFrmGiftBatch gb = new TFrmGiftBatch(this);
                     gb.ViewMode = true;
                     gb.ViewModeTDS = FMainDS;
-                    gb.LedgerNumber = FLedgerNumber;
-                    gb.FindGiftDetail(FPreviouslySelectedDetailRow);
+                     // When I call Gift Batch, it will want one row in a LedgerTable!
+                    gb.ViewModeTDS.ALedger.Merge(TRemote.MFinance.AP.WebConnectors.GetLedgerInfo(FLedgerNumber)); 
+
+                    gb.ShowDetailsOfOneBatch(FLedgerNumber, FPreviouslySelectedDetailRow.BatchNumber);
+
+//                    gb.LoadTransactions(FLedgerNumber, FPreviouslySelectedDetailRow.BatchNumber, MFinanceConstants.BATCH_POSTED);
+
+                    // To call this I need to set FBatchRow, and this will require FBatchNumber to be set.
+//                    gb.FindGiftDetail(FPreviouslySelectedDetailRow);
+//                    gb.EnableTransactions();
                     gb.Show();
                 }
                 finally
