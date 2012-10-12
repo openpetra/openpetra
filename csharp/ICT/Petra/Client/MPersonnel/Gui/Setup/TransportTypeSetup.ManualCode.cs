4a3b41f34d339d71be83bb2556ed58489aa11669
Alan Paterson <alan.paterson@om.org>
2012-10-12 12:22:25 +0100

    Added new MT Screen for TransportTypes (TravelType).  It goes in Personnel Setup Application-related

---
+++ csharp/ICT/Petra/Client/MPersonnel/Gui/Setup/TransportTypeSetup.ManualCode.cs
@@ -0,0 +1,93 @@
+﻿//
+// DO NOT REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+//
+// @Authors:
+//       alanp
+//
+// Copyright 2004-2012 by OM International
+//
+// This file is part of OpenPetra.org.
+//
+// OpenPetra.org is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenPetra.org is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenPetra.org.  If not, see <http://www.gnu.org/licenses/>.
+//
+using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Xml;
+using GNU.Gettext;
+using Ict.Common.Verification;
+using Ict.Common;
+using Ict.Common.IO;
+using Ict.Petra.Client.App.Core.RemoteObjects;
+using Ict.Petra.Shared.MPersonnel;
+using Ict.Petra.Shared.MPersonnel.Personnel.Data;
+using Ict.Petra.Shared.MCommon.Validation;
+
+namespace Ict.Petra.Client.MPersonnel.Gui.Setup
+{
+    public partial class TFrmTransportTypeSetup
+    {
+        private void RunOnceOnActivationManual()
+        {
+            chkDetailDeletableFlag.Enabled = false;
+        }
+
+        private void NewRowManual(ref PtTravelTypeRow ARow)
+        {
+            string baseCode = Catalog.GetString("NEWCODE");
+            string newCode = baseCode;
+            int suffix = 1;
+
+            while (FMainDS.PtTravelType.Rows.Find(new object[] { newCode }) != null)
+            {
+                newCode = baseCode + suffix.ToString();
+                suffix++;
+            }
+
+            ARow.Code = newCode;
+        }
+
+        private void NewRecord(Object sender, EventArgs e)
+        {
+            CreateNewPtTravelType();
+        }
+
+        private void GetDetailDataFromControlsManual(PtTravelTypeRow ARow)
+        {
+        }
+
+        private void EnableDisableUnassignableDate(Object sender, EventArgs e)
+        {
+            dtpDetailUnassignableDate.Enabled = chkDetailUnassignableFlag.Checked;
+
+            if (!chkDetailUnassignableFlag.Checked)
+            {
+                dtpDetailUnassignableDate.Date = null;
+            }
+            else
+            {
+                dtpDetailUnassignableDate.Date = DateTime.Now.Date;
+            }
+        }
+
+        private void ValidateDataDetailsManual(PtTravelTypeRow ARow)
+        {
+            TVerificationResultCollection VerificationResultCollection = FPetraUtilsObject.VerificationResultCollection;
+
+            TSharedValidation_CacheableDataTables.ValidateTransportType(this, ARow, ref VerificationResultCollection,
+                FPetraUtilsObject.ValidationControlsDict);
+        }
+    }
+}
