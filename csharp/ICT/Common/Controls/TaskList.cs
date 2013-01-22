e1ac048d99533e899a7523a39c1f22bc90a9934a
christiank <christian.kendel@om.org>
2013-01-22 06:40:44 +0100

    TaskList Control - non-functional changes: Added Argument checks to 'GetAttribute' and 'AttributeTrue' Methods. Removed a line of code from Method 'AttributeFalse' that didn't do anything.

--- csharp/ICT/Common/Controls/TaskList.cs
+++ csharp/ICT/Common/Controls/TaskList.cs
@@ -657,6 +657,16 @@ namespace Ict.Common.Controls
         /// <returns>string attribute, or empty string if attribute is null.</returns>
         public string GetAttribute(XmlNode node, string attr)
         {
+            if (node == null) 
+            {
+                throw new ArgumentNullException("Argument 'node' must not be null");
+            }
+            
+            if (node.Attributes == null) 
+            {
+                throw new ArgumentNullException("Argument 'node' must have Attributes (node.Name='" + node.Name + "')");
+            }
+            
             if (node.Attributes[attr] != null)
             {
                 return node.Attributes[attr].Value;
@@ -676,8 +686,16 @@ namespace Ict.Common.Controls
         /// <returns>Boolean whether given Xml Node has the passed attribute set to true</returns>
         public bool AttributeTrue(XmlNode node, string attr, bool TrueByDefault = true)
         {
-            XmlAttributeCollection tmp = node.Attributes;
-
+            if (node == null) 
+            {
+                throw new ArgumentNullException("Argument 'node' must not be null");
+            }
+            
+            if (node.Attributes == null) 
+            {
+                throw new ArgumentNullException("Argument 'node' must have Attributes (node.Name='" + node.Name + "')");
+            }
+            
             if (node.Attributes[attr] == null)
             {
                 if (TrueByDefault)
@@ -701,8 +719,6 @@ namespace Ict.Common.Controls
         /// <returns>Boolean whether given Xml Node has the passed attribute set to true</returns>
         public bool AttributeFalse(XmlNode node, string attr)
         {
-            XmlAttributeCollection tmp = node.Attributes;
-
             if (node.Attributes[attr] == null)
             {
                 return false;
