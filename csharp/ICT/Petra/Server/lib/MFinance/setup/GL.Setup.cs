aad25175f4fd7a69dcd0ab65ee371a0cdd0fd924
Alan Paterson <alan.paterson@om.org>
2013-01-23 09:34:51 +0000

    Added two more aLedgerTable fields to those returned by GetAvailableLedgers() - we now can read the ledger status (is it in use?) and the base ledger currency - in addition to the ledger number and name.  This is needed by both the exchange rate screens to try and decide a likely candidate for the relevant base currency

--- csharp/ICT/Petra/Server/lib/MFinance/setup/GL.Setup.cs
+++ csharp/ICT/Petra/Server/lib/MFinance/setup/GL.Setup.cs
@@ -1427,6 +1427,8 @@ namespace Ict.Petra.Server.MFinance.Setup.WebConnectors
 
             Fields.Add(ALedgerTable.GetLedgerNameDBName());
             Fields.Add(ALedgerTable.GetLedgerNumberDBName());
+            Fields.Add(ALedgerTable.GetBaseCurrencyDBName());
+            Fields.Add(ALedgerTable.GetLedgerStatusDBName());
             return ALedgerAccess.LoadAll(Fields, null, null, 0, 0);
         }
 
