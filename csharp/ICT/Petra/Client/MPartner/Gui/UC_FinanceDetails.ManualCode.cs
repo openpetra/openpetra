a0cd0c1c43c000ee2dfeec37e50192747d4fb72d
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2013-02-14 08:23:35 +0100

    fix small compiler error to work with code from trunk

--- csharp/ICT/Petra/Client/MPartner/Gui/UC_FinanceDetails.ManualCode.cs
+++ csharp/ICT/Petra/Client/MPartner/Gui/UC_FinanceDetails.ManualCode.cs
@@ -4,7 +4,7 @@
 // @Authors:
 //       joachimm, timop
 //
-// Copyright 2004-2012 by OM International
+// Copyright 2004-2013 by OM International
 //
 // This file is part of OpenPetra.org.
 //
@@ -30,7 +30,7 @@ using Ict.Common.Remoting.Client;
 using Ict.Petra.Client.App.Core;
 using Ict.Petra.Client.MPartner;
 using Ict.Petra.Shared;
-using Ict.Petra.Shared.Interfaces.MPartner.Partner.UIConnectors;
+using Ict.Petra.Shared.Interfaces.MPartner;
 using Ict.Petra.Shared.MCommon;
 using Ict.Petra.Shared.MCommon.Data;
 using Ict.Petra.Shared.MPartner;
