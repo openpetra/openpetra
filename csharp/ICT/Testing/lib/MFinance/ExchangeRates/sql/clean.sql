6a68e379ca60bacb189d6f13b43ff0ad42e32a6f
christiank <christian.kendel@om.org>
2013-03-01 10:38:05 +0100

    Fix broken Unit Tests for Daily Exchange Rates: three SQL files were missing from Alan's commit which contained that NUnit Test. Include the DLL that contains those tests again in the ICT.Testing.build file.

---
+++ csharp/ICT/Testing/lib/MFinance/ExchangeRates/sql/clean.sql
@@ -0,0 +1,8 @@
+﻿DELETE FROM a_journal WHERE a_ledger_number_i=9997;
+DELETE FROM a_batch WHERE a_ledger_number_i=9997;
+
+DELETE FROM a_gift_batch WHERE a_ledger_number_i=9997;
+DELETE FROM a_cost_centre WHERE a_ledger_number_i=9997;
+DELETE FROM a_cost_centre_types WHERE a_ledger_number_i=9997;
+DELETE FROM a_account WHERE a_ledger_number_i=9997;
+DELETE FROM a_ledger WHERE a_ledger_number_i=9997;
