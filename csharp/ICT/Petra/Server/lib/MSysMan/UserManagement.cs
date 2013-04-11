ace03d8aa056a8fcde2da7fb2966afdcf1907b57
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2013-04-11 14:44:01 +0200

    fix problem with changing user permissions

--- csharp/ICT/Petra/Server/lib/MSysMan/UserManagement.cs
+++ csharp/ICT/Petra/Server/lib/MSysMan/UserManagement.cs
@@ -420,12 +420,15 @@ namespace Ict.Petra.Server.MSysMan.Maintenance.WebConnectors
             // TODO: if user module access permissions have changed, automatically update the table access permissions?
 
             // for new users: create users on the alternative authentication method
-            foreach (SUserRow user in ASubmitDS.SUser.Rows)
+            if (ASubmitDS.SUser != null)
             {
-                if (user.RowState == DataRowState.Added)
+                foreach (SUserRow user in ASubmitDS.SUser.Rows)
                 {
-                    CreateUser(user.UserId, string.Empty, user.FirstName, user.LastName, string.Empty);
-                    user.AcceptChanges();
+                    if (user.RowState == DataRowState.Added)
+                    {
+                        CreateUser(user.UserId, string.Empty, user.FirstName, user.LastName, string.Empty);
+                        user.AcceptChanges();
+                    }
                 }
             }
 
