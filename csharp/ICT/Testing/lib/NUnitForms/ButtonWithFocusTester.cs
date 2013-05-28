5a5957518d002293841c2520e9ba40c2da74c7b6
Alan Paterson <alan.paterson@om.org>
2013-05-28 09:16:40 +0100

    1.  One of the rows was being modified during the DeleteRows test when it should have remained unchanged.  This was due to the ButtonTester Click method only calling the click code, whereas it really needs to focus the button first and then call the code.  Rather than add a .Focus line before every click I have created a ButtonWithFocusTester sub-class where the Click method does that job. 2.  The DeleteRows test was succeeding but the TearDown method was failing due to concurrency because itt was working with a data set that had become out of date.  TearDown now re-loads the data set. 3.  While debugging the issues in (1) above I added a number of new Assert's.  I have left these in place, but they are not part of the fix per se.

---
+++ csharp/ICT/Testing/lib/NUnitForms/ButtonWithFocusTester.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NUnit.Extensions.Forms;
+
+namespace Ict.Testing.NUnitForms
+{
+    /// <summary>
+    /// A class derived from the standard ButtonTester.  The only difference is that the button takes the focus before the click event is fired.
+    /// This makes it behave more like a real button.
+    /// </summary>
+    public class ButtonWithFocusTester : ButtonTester
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name">Name of button</param>
+        public ButtonWithFocusTester(string name)
+            : base(name)
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="tester"></param>
+        /// <param name="index"></param>
+        public ButtonWithFocusTester(ButtonTester tester, int index)
+            : base(tester, index)
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="form"></param>
+        public ButtonWithFocusTester(string name, Form form)
+            : base(name, form)
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="formName"></param>
+        public ButtonWithFocusTester(string name, string formName)
+            : base(name, formName)
+        {
+        }
+
+        /// <summary>
+        /// The click event is  fired after the button has been given the focus
+        /// </summary>
+        public override void Click()
+        {
+            Properties.Focus();
+            base.Click();
+        }
+    }
+}
