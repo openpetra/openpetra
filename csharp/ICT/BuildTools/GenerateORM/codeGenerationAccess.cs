d01e389349e1db92ec7f38dc34027b5ff7d05030
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2013-10-31 09:47:50 +0100

    nant generateORM now creates files with -generated.cs in the name. this helps with nant quickClean, and should make nant uncrustify simpler as well

--- csharp/ICT/BuildTools/GenerateORM/codeGenerationAccess.cs
+++ csharp/ICT/BuildTools/GenerateORM/codeGenerationAccess.cs
@@ -4,7 +4,7 @@
 // @Authors:
 //       timop
 //
-// Copyright 2004-2012 by OM International
+// Copyright 2004-2013 by OM International
 //
 // This file is part of OpenPetra.org.
 //
@@ -737,7 +737,7 @@ namespace Ict.Tools.CodeGeneration.DataStore
                 }
             }
 
-            Template.FinishWriting(AFilePath + AFilename + ".cs", ".cs", true);
+            Template.FinishWriting(AFilePath + AFilename + "-generated.cs", ".cs", true);
 
             return true;
         }
