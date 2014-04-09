74509f0fd90937fbbe362f1af1d384e28d51171e
tim.ingham <tim.ingham@om.org>
2014-04-09 15:48:18 +0100

    0002962: Income & Expense Report - Account Level report very slow

--- csharp/ICT/Petra/Server/lib/MFinance/GL/Reporting.UIConnectors.cs
+++ csharp/ICT/Petra/Server/lib/MFinance/GL/Reporting.UIConnectors.cs
@@ -1220,11 +1220,13 @@ namespace Ict.Petra.Server.MFinance.Reporting.WebConnectors
                     DataView OldPeriod = new DataView(resultTable);
                     DataView ThisMonth = new DataView(resultTable);
                     ThisMonth.RowFilter = "Period=" + ReportPeriodEnd;
+                    OldPeriod.Sort = "Year,Period,CostCentreCode,AccountCode";
 
                     //
                     // If I have rows for the previous month too, I can subtract the previous month's YTD balance to get my "Actual".
                     if (ReportPeriodEnd > PeriodMonths)
                     {
+                        Int32 PrevPeriod = ReportPeriodEnd - PeriodMonths;
                         foreach (DataRowView rv in ThisMonth)
                         {
                             if (DbAdapter.IsCancelled)
@@ -1233,13 +1235,15 @@ namespace Ict.Petra.Server.MFinance.Reporting.WebConnectors
                             }
 
                             DataRow Row = rv.Row;
-                            OldPeriod.RowFilter = String.Format("Year={0} AND Period={1} AND CostCentreCode='{2}' AND AccountCode='{3}'",
-                                Convert.ToInt32(Row["Year"]),
-                                ReportPeriodEnd - PeriodMonths,
-                                Row["CostCentreCode"].ToString(),
-                                Row["AccountCode"].ToString()
+                            Int32 RowIdx = OldPeriod.Find(
+                                new Object []{
+                                    Row["Year"],
+                                    PrevPeriod,
+                                    Row["CostCentreCode"],
+                                    Row["AccountCode"]
+                                }
                                 );
-                            DataRow PreviousPeriodRow = OldPeriod[0].Row;
+                            DataRow PreviousPeriodRow = OldPeriod[RowIdx].Row;
                             Row["Actual"] = Convert.ToDecimal(Row["ActualYTD"]) - Convert.ToDecimal(PreviousPeriodRow["ActualYTD"]);
                         }
                     }
@@ -1272,16 +1276,18 @@ namespace Ict.Petra.Server.MFinance.Reporting.WebConnectors
                         }
 
                         DataRow Row = rv.Row;
-                        OldPeriod.RowFilter = String.Format("Year={0} AND Period={1} AND CostCentreCode='{2}' AND AccountCode='{3}'",
-                            AccountingYear - 1,
-                            ReportPeriodEnd,
-                            Row["CostCentreCode"].ToString(),
-                            Row["AccountCode"].ToString()
+                        Int32 RowIdx = OldPeriod.Find(
+                            new Object []{
+                                AccountingYear - 1,
+                                ReportPeriodEnd,
+                                Row["CostCentreCode"],
+                                Row["AccountCode"]
+                                }
                             );
 
-                        if (OldPeriod.Count > 0)
+                        if (RowIdx >= 0)
                         {
-                            DataRow LastYearRow = OldPeriod[0].Row;
+                            DataRow LastYearRow = OldPeriod[RowIdx].Row;
                             Row["ActualLastYear"] = Convert.ToDecimal(LastYearRow["Actual"]);
                             Row["BudgetLastYear"] = Convert.ToDecimal(LastYearRow["Budget"]);
                         }
