81499360c850d1e4229b8bb558cf77b126f3b9bc
christiank <christian.kendel@om.org>
2014-07-15 15:43:27 +0200

    Fixes for Partner Key highlighting and Partner Class highlighting: * Method 'SetPartnerKeyBackColour': assign 'White' as BackColour, not System.Drawing.SystemColors.Control for Partner Classes other than PERSON. The difference between those two colours is very apparent on Windows Server and White is OK, whereas System.Drawing.SystemColors.Control makes the Control appear 'read-only', where it isn't. On Windows 7 the difference was less apparent. * Partner Edit Screen: Highlight the Partner Class in yellow for PERSONs. This was orinally done in Petra 2.x, then got removed in OpenPetra by request. It is now put in again as Personnel users really want that highlighting...

--- csharp/ICT/Petra/Client/MPartner/Gui/UC_PartnerEdit_TopPart.ManualCode.cs
+++ csharp/ICT/Petra/Client/MPartner/Gui/UC_PartnerEdit_TopPart.ManualCode.cs
@@ -160,6 +160,8 @@ namespace Ict.Petra.Client.MPartner.Gui
                     txtPersonFamilyName.TextChanged += new EventHandler(OnAnyDataColumnChanging);
                     this.cmbPersonGender.SelectedValueChanged += new System.EventHandler(this.CmbPersonGender_SelectedValueChanged);
 
+                    txtPartnerClass.BackColor = System.Drawing.Color.Yellow;
+                    
                     break;
 
                 case TPartnerClass.FAMILY:
