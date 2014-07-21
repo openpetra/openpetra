d351518e92b590dd68d26c777ad017eacb9cf837
tim.ingham <tim.ingham@om.org>
2014-07-21 12:53:30 +0100

    0003212: Account hierarchy - print chart of accounts: It prints inactive analysis attributes, which is misleading

--- XmlReports/Backup_Account Hierarchy_5.sql
+++ XmlReports/Backup_Account Hierarchy_5.sql
@@ -2,7 +2,7 @@ DELETE FROM s_report_template WHERE s_template_id_i=5;
 INSERT INTO s_report_template (s_template_id_i,s_report_type_c,s_report_variant_c,s_author_c,s_default_l,s_readonly_l,s_private_l,s_private_default_l,s_xml_text_c)
 VALUES(5,'Account Hierarchy','Account Hierarchy template','System',True,False,False,False,
 '﻿<?xml version="1.0" encoding="utf-8"?>
-<Report ScriptLanguage="CSharp" DoublePass="true" ReportInfo.Created="11/05/2013 15:46:27" ReportInfo.Modified="07/07/2014 12:32:44" ReportInfo.CreatorVersion="2014.2.1.0">
+<Report ScriptLanguage="CSharp" DoublePass="true" ReportInfo.Created="11/05/2013 15:46:27" ReportInfo.Modified="07/21/2014 12:52:51" ReportInfo.CreatorVersion="2014.2.1.0">
   <ScriptText>using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -121,7 +121,11 @@ namespace FastReport
       <TextObject Name="Text3" Width="37.8" Height="15.12" Text="[AccountHierarchy.AccountLevel]" TextFill.Color="White"/>
       <TextObject Name="Text4" Left="425.25" Width="207.9" Height="15.12" Font="Arial, 9pt" TextFill.Color="DarkGreen"/>
       <DataBand Name="Data1" Top="59.59" Width="718.2" Height="15.12" DataSource="AnalysisAttribute" Filter="[AccountHierarchy.a_account_code_c]==[AnalysisAttribute.a_account_code_c]">
-        <TextObject Name="Text2" Left="425.25" Width="207.9" Height="15.12" Text="[AnalysisAttribute.a_analysis_type_code_c]" Font="Arial, 9pt" TextFill.Color="DarkGreen"/>
+        <TextObject Name="Text2" Left="425.25" Width="207.9" Height="15.12" Text="[AnalysisAttribute.a_analysis_type_code_c]" Font="Arial, 9pt" TextFill.Color="DarkGreen">
+          <Highlight>
+            <Condition Expression="![AnalysisAttribute.a_active_l]" TextFill.Color="DarkGray" Font="Arial, 9.75pt, style=Strikeout" ApplyFont="true"/>
+          </Highlight>
+        </TextObject>
         <TextObject Name="Text43" Left="160.65" Width="264.6" Height="15.12" Font="Arial, 9pt" TextFill.Color="DarkGreen"/>
         <TextObject Name="Text44" Left="37.8" Width="122.85" Height="15.12" Font="Arial, 9pt" TextFill.Color="DarkGreen"/>
       </DataBand>
