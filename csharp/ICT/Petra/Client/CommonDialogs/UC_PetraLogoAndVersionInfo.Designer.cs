a01d9ff98705811a985172485f6e47a0f63e3c6c
christiank <christian.kendel@om.org>
2014-07-22 13:12:48 +0200

    For Release 0.3.0: change '2013' to '2014' in the copyright notice.

--- csharp/ICT/Petra/Client/CommonDialogs/UC_PetraLogoAndVersionInfo.Designer.cs
+++ csharp/ICT/Petra/Client/CommonDialogs/UC_PetraLogoAndVersionInfo.Designer.cs
@@ -138,7 +138,7 @@ namespace Ict.Petra.Client.CommonDialogs
             this.lblCopyrightNotice.Name = "lblCopyrightNotice";
             this.lblCopyrightNotice.Size = new System.Drawing.Size(238, 18);
             this.lblCopyrightNotice.TabIndex = 8;
-            this.lblCopyrightNotice.Text = "© 1995 - 2013 by OM International";
+            this.lblCopyrightNotice.Text = "© 1995 - 2014 by OM International";
             this.lblCopyrightNotice.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             //
             // lblInstallationKind
