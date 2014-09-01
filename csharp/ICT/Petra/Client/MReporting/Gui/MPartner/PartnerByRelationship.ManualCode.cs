373ff4d7ff019d996067ead9f88fcfb696a76335
Andrew <andrew.r.webster7@gmail.com>
2014-09-01 13:56:51 +0100

    displaying check-list of relationship types

--- csharp/ICT/Petra/Client/MReporting/Gui/MPartner/PartnerByRelationship.ManualCode.cs
+++ csharp/ICT/Petra/Client/MReporting/Gui/MPartner/PartnerByRelationship.ManualCode.cs
@@ -28,35 +28,42 @@ using Ict.Petra.Client.CommonControls;
 using Owf.Controls;
 using Ict.Petra.Client.App.Core.RemoteObjects;
 using Ict.Petra.Shared;
+using System.Data;
+using Ict.Petra.Client.App.Core;
+using Ict.Petra.Shared.MPartner.Partner.Data;
+using Ict.Common;
 
 namespace Ict.Petra.Client.MReporting.Gui.MPartner
 {
     public partial class TFrmPartnerByRelationship
     {
-        private void InitializeManualCode(){}
-
-        private void PartnerChanged(Int64 APartnerKey, String APartnerShortName, bool AValidSelection)
+        private void InitializeManualCode()
         {
-            if (AValidSelection)
-            {
-                //TPartnerClass foo;
-                //TRemote.MPartner.Partner.ServerLookups.WebConnectors.GetPartnerShortName(APartnerKey, out APartnerShortName, out foo);
+            string CheckedMember = "CHECKED";
+            string DisplayMember = "p_relation_description_c";//PTypeTable.GetTypeDescriptionDBName();
+            string ValueMember = "p_relation_name_c";//PTypeTable.GetTypeCodeDBName();
 
-                //string AllowedPartnerClasses = FFromPartnerClass.ToString();
-                //AllowedDifferentPartnerClasses(ref AllowedPartnerClasses, FFromPartnerClass);
+            DataTable table = TDataCache.TMPartner.GetCacheablePartnerTable(TCacheablePartnerTablesEnum.RelationList);
+            DataView view = new DataView(table);
 
-                //// restrict the choice of partner class for txtMergeTo
-                //txtMergeTo.PartnerClass = AllowedPartnerClasses;
+            view.Sort = ValueMember;
 
-                //if ((txtMergeTo.Text != "0000000000") && (APartnerKey != Convert.ToInt64(txtMergeTo.Text)))
-                //{
-                //    btnOK.Enabled = true;
-                //}
-                //else
-                //{
-                //    btnOK.Enabled = false;
-                //}
-            }
+            var newTable = view.ToTable(true, new string[] { ValueMember, DisplayMember });
+            newTable.Columns.Add(new DataColumn(CheckedMember, typeof(bool)));
+            
+            clbIncludeRelationships.SpecialKeys = (SourceGrid.GridSpecialKeys)(
+                SourceGrid.GridSpecialKeys.Arrows |
+                SourceGrid.GridSpecialKeys.PageDownUp |
+                SourceGrid.GridSpecialKeys.Enter |
+                SourceGrid.GridSpecialKeys.Escape |
+                SourceGrid.GridSpecialKeys.Control | 
+                SourceGrid.GridSpecialKeys.Shift);
+            clbIncludeRelationships.Columns.Clear();
+            clbIncludeRelationships.AddCheckBoxColumn("", newTable.Columns[CheckedMember], 17, false);
+            clbIncludeRelationships.AddTextColumn(Catalog.GetString("Type Code"), newTable.Columns[ValueMember], 100);
+            clbIncludeRelationships.AddTextColumn(Catalog.GetString("Type Description"), newTable.Columns[DisplayMember], 320);
+            clbIncludeRelationships.DataBindGrid(newTable, ValueMember, CheckedMember, ValueMember, false, true, false);
         }
+
     }
 }
