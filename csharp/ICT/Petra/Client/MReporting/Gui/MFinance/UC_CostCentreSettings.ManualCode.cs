221d96d6a31c4c62119a2040f8531fc29f4e9ac3
peter.simpson <peter.simpson@om.org>
2014-09-01 15:50:42 +0100

    Various small bug fixes.

--- csharp/ICT/Petra/Client/MReporting/Gui/MFinance/UC_CostCentreSettings.ManualCode.cs
+++ csharp/ICT/Petra/Client/MReporting/Gui/MFinance/UC_CostCentreSettings.ManualCode.cs
@@ -51,6 +51,9 @@ namespace Ict.Petra.Client.MReporting.Gui.MFinance
         public void InitialiseData(TFrmPetraReportingUtils APetraUtilsObject)
         {
             FCostCenterCodesDuringLoad = "";
+            
+            // otherwise rbtSelectedCostCentres slightly obscures clbCostCentres
+            rbtSelectedCostCentres.SendToBack();
         }
 
         #region Parameter/Settings Handling
