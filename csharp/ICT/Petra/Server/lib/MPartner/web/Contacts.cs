6dc21607ea91093f31f580bc52eb175362a29841
Alan Paterson <alan.paterson@om.org>
2014-11-17 11:02:40 +0000

    Get rid of all Jenkins warnings

--- csharp/ICT/Petra/Server/lib/MPartner/web/Contacts.cs
+++ csharp/ICT/Petra/Server/lib/MPartner/web/Contacts.cs
@@ -274,9 +274,6 @@ namespace Ict.Petra.Server.MPartner.Partner.WebConnectors
 
             try
             {
-                PContactLogTable TempTable = new PContactLogTable();
-                PContactLogRow TemplateRow = TempTable.NewRowTyped(false);
-
                 contacts = PContactLogAccess.LoadViaPPartnerPPartnerContact(partnerKey, WriteTransaction);
             }
             catch (Exception e)
@@ -309,9 +306,6 @@ namespace Ict.Petra.Server.MPartner.Partner.WebConnectors
 
             try
             {
-                PContactLogTable TempTable = new PContactLogTable();
-                PContactLogRow TemplateRow = TempTable.NewRowTyped(false);
-
                 partnerContacts = PPartnerContactAccess.LoadViaPPartner(partnerKey, WriteTransaction);
             }
             catch (Exception e)
