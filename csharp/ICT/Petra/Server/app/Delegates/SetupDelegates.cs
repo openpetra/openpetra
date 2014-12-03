40d2072ae9ccad3e19f67227a291d0e58c681fcf
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-12-03 20:38:42 +0100

    fix delegate for posting register. avoid error after posting. this was not merged from lp:openpetraorg because the SetupDelegates.cs file has been moved and renamed

--- csharp/ICT/Petra/Server/app/Delegates/SetupDelegates.cs
+++ csharp/ICT/Petra/Server/app/Delegates/SetupDelegates.cs
@@ -49,6 +49,8 @@ using Ict.Petra.Server.MPartner;
 using Ict.Petra.Shared.MPersonnel.Personnel.Data;
 using Ict.Petra.Server.MFinance.GL;
 using Ict.Petra.Server.MFinance.ICH.WebConnectors;
+using Ict.Petra.Server.MReporting.WebConnectors;
+using Ict.Petra.Server.MFinance.Common;
 
 namespace Ict.Petra.Server.App.Delegates
 {
@@ -83,6 +85,7 @@ namespace Ict.Petra.Server.App.Delegates
             TSharedPartnerValidationHelper.VerifyPartnerDelegate = @TPartnerServerLookups.VerifyPartner;
             TSharedFinanceValidationHelper.GetValidPostingDateRangeDelegate = @TFinanceServerLookups.GetCurrentPostingRangeDates;
             TMonthEnd.StewardshipCalculationDelegate = @TStewardshipCalculationWebConnector.PerformStewardshipCalculation;
+            TGLPosting.PrintReportOnClientDelegate = @TReportingWebConnector.GenerateReportOnClient;
 
             // Set up Delegates for retrieval of cacheable tables when called from Shared directories on server side
             CachePopulatorCommon = new Ict.Petra.Server.MCommon.Cacheable.TCacheable();
