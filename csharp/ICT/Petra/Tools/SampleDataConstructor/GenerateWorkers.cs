0f7fffbc174ee46caf0255cb4ca9f7dd8f0bf629
Christian Kendel <christian.kendel@om.org>
2014-12-30 14:45:36 +0100

    Fix some Compiler Warnings when compiling with mono on Linux.

--- csharp/ICT/Petra/Tools/SampleDataConstructor/GenerateWorkers.cs
+++ csharp/ICT/Petra/Tools/SampleDataConstructor/GenerateWorkers.cs
@@ -337,17 +337,6 @@ namespace Ict.Petra.Tools.SampleDataConstructor
                 personLocationRow.LocationType = MPartnerConstants.LOCATIONTYPE_HOME;
                 personLocationRow.SendMail = true;
                 AMainDS.PPartnerLocation.Rows.Add(personLocationRow);
-
-                string prefix = "Male";
-
-                if (countPerson == 1)
-                {
-                    prefix = "Female";
-                }
-                else if (countPerson > 1)
-                {
-                    prefix = "Child" + (countPerson - 1).ToString();
-                }
             }
         }
 
