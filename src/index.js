0e18eb581576ba31df58d6d8adc49aa70f0d7174
Timotheus Pokorra <tp@tbits.net>
2018-02-27 07:48:54 +0100

    use npm and browserify to bundle the external dependencies. update for the navigation sidebar, works now on mobile phone

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 // @Authors:
 //	   Timotheus Pokorra <tp@tbits.net>
 //
-// Copyright 2017 by TBits.net
+// Copyright 2017-2018 by TBits.net
 //
 // This file is part of OpenPetra.
 //
@@ -22,49 +22,14 @@
 // along with OpenPetra.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-// call the server regularly to keep the connection open
-function keepConnection() {
-	api.post('serverSessionManager.asmx/PollClientTasks')
-		.then(function(response) {
-			// console.log("keepConnection call succeeded");
-		})
-		.catch(function(error) {
-			console.log(error);
-		});
-
-	// call every 30 seconds
-	setTimeout(keepConnection, 30000);
-}
-
-auth = new Auth();
-
-auth.checkAuth(function() {
-	$("#loading").hide();
-	$("#login").show();
-	$("#btnLogin").click(function(e) {
-		e.preventDefault();
-		user=$("#txtEmail").val();
-		pwd=$("#txtPassword").val();
-		if (user == "" && pwd == "") {
-			user = "demo";
-			pwd = "demo";
-		}
-		auth.login(user, pwd);
-	});
-}, function () {
-	setTimeout(keepConnection, 5000);
-
-	// load the navigation bars now.
-	// we don't want the navigation bars displayed if the user is not logged in
-	$("#loading").hide();
-	$("#main").show();
-	nav = new Navigation();
-	nav.loadNavigation();
-
-	$("#logout").click(function(e) {
-			var stateObj = { "logout": "done" };
-			history.pushState(stateObj, "OpenPetra", "/");
-			e.preventDefault();
-			auth.logout();
-		});
-});
+$ = require('jquery');
+require('bootstrap');
+i18next = require('i18next');
+axios = require('axios');
+i18nextXHRBackend = require('i18next-xhr-backend');
+i18nextBrowserLanguageDetector = require('i18next-browser-languagedetector');
+//require('./lib/ajax.js');
+//require('./lib/auth.js');
+//require('./lib/i18n.js');
+//require('./lib/navigation.js');
+//require('./lib/app.js');
