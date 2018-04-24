5a7c37fe1a3658af49cb6dfc92cd7422317307a0
Timotheus Pokorra <tp@tbits.net>
2018-04-24 07:31:18 +0200

    generateGlue: support List of strings as parameters

--- csharp/ICT/Common/Remoting/Shared/Serialization.cs
+++ csharp/ICT/Common/Remoting/Shared/Serialization.cs
@@ -321,7 +321,7 @@ namespace Ict.Common.Remoting.Shared
 
             if ((type == "System.String") && s.EndsWith(":base64"))
             {
-                return System.Text.UTF8Encoding.ASCII.GetString(Convert.FromBase64String(s.Substring(0, s.Length - ":base65".Length)));
+                return System.Text.UTF8Encoding.ASCII.GetString(Convert.FromBase64String(s.Substring(0, s.Length - ":base64".Length)));
             }
 
             if (s.EndsWith(":binary"))
@@ -406,6 +406,32 @@ namespace Ict.Common.Remoting.Shared
             {
                 return null;
             }
+        }
+
+        /// <summary>
+        /// Deserialize List of String, that might be base64 encoded
+        /// </summary>
+        static public List<string> DeserializeObject(List<string> l)
+        {
+            if (l == null)
+            {
+                return null;
+            }
+
+            List<string> result = new List<string>();
+
+            foreach (string s in l) {
+                if (s.EndsWith(":base64"))
+                {
+                    result.Add(System.Text.UTF8Encoding.ASCII.GetString(Convert.FromBase64String(s.Substring(0, s.Length - ":base64".Length))));
+                }
+                else
+                {
+                    result.Add(s);
+                }
+            }
+
+            return result;
         }
     }
 }
