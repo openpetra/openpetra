fba21458e6bdf1e14aa1f83439e337a58534fd3b
The_CJ <cj@tbits.net>
2018-04-24 10:57:38 +0200

    added a global message funtion

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -12,3 +12,42 @@ function API_call(url, args, next_function) {
   return r;
 
 }
+
+function display_message(content, style_arguments) {
+  var display_space = $('#global_message_space');
+  if (display_space.length == 0) {
+    let x = $('<div id ="global_message_space" class="text-center" style="position:fixed;top:10vh;width:100%;padding:20px;z-index:5000;">');
+    $('body').append(x);
+  }
+  var message = $('<div class="text-center msg" style="width:50%;margin:5px auto;cursor:pointer;" onclick="$(this).closest(\'.msg\').remove()">');
+  message.addClass('display_message');
+
+  if (typeof style_arguments == "string") {
+    if (style_arguments == "success") {
+      message.addClass('display_message_success');
+    }
+    if (style_arguments == "fail") {
+      message.addClass('display_message_fail');
+    }
+  }
+
+  if (style_arguments == null) {
+    style_arguments = {};
+  }
+
+  for (var arg in style_arguments) {
+    message.css(arg, style_arguments[arg]);
+  }
+
+  message.text(content);
+
+  var m_id = Math.floor(Math.random() * 100000);
+  message.attr('message-id', m_id);
+
+  $('#global_message_space').append(message);
+
+  setTimeout(function () {
+    $('[message-id='+m_id+']').remove();
+  }, 30000);
+
+}
