133beb28ebfaf6456c080520d3366ead864883be
Timotheus Pokorra <tp@tbits.net>
2018-07-09 20:46:11 +0200

    update version to 2017.07, and increase copyright year

--- inc/template/src/AssemblyInfo.cs
+++ inc/template/src/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OM International")]
 [assembly: AssemblyProduct("OpenPetra")]
-[assembly: AssemblyCopyright("(c) OM International 2004-2017")]
+[assembly: AssemblyCopyright("(c) OM International 2004-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
