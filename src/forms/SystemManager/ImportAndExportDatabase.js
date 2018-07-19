a195ecfe8bd6804e68434b8a7acc915fbedd88a5
root <root@002-openpetra.tbits.net>
2018-07-19 15:21:43 +0200

    added button to upload partner csv

--- src/forms/SystemManager/ImportAndExportDatabase.js
+++ src/forms/SystemManager/ImportAndExportDatabase.js
@@ -110,6 +110,44 @@ function ResetDatabase(self) {
 	reader.readAsDataURL(self[0].files[0]);
 };
 
+function UploadUserCSV(self) {
+
+	var filename = self.val();
+
+	// see http://www.html5rocks.com/en/tutorials/file/dndfiles/
+	if (window.File && window.FileReader && window.FileList && window.Blob) {
+		//alert("Great success! All the File APIs are supported.");
+	} else {
+	  alert('The File APIs are not fully supported in this browser.');
+	}
+
+	var reader = new FileReader();
+
+	reader.onload = function (event) {
+		p = {'ACSVPartnerData': event.target.result, 'ADateFormat': "dmy", "ASeparator": ";"};
+
+		api.post('serverMSysMan.asmx/TImportExportWebConnector_ImportFromCSVFile', p)
+		.then(function (result) {
+			result = result.data;
+			if (result != '') {
+				if (result.d == true) {
+					display_message(i18next.t('ImportAndExportDatabase.upload_partner_success'), "success");
+				} else {
+					display_message(i18next.t('ImportAndExportDatabase.upload_partner_fail'), "fail");
+				}
+			}
+		})
+		.catch(error => {
+			//console.log(error.response)
+			display_message(i18next.t('ImportAndExportDatabase.upload_partner_fail'), "fail");
+		});
+
+	}
+	// Read in the file as a data URL.
+	reader.readAsText(self[0].files[0]);
+
+};
+
 function showPleaseWait() {
 	$('#myModal').modal();
 }
