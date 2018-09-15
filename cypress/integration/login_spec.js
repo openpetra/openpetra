4f68122eee58650b13c85a64b671d6c46345d736
Timotheus Pokorra <tp@tbits.net>
2018-09-15 07:11:17 +0200

    improve test: wait for buttons or input to be visible works for initial run as well, when isUserLoggedIn takes a bit longer

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -1,26 +1,25 @@
 describe('simple login', function() {
   it('logs in with user demo', function() {
     cy.visit('/')
+    cy.get('#txtEmail').should("be.visible")
     cy.get('#txtEmail').type('demo')
     cy.get('#txtPassword').type('demo')
-    cy.server()
-    cy.route("POST", "/api/serverSessionManager.asmx/GetVersion", []).as("login")
     cy.get('#btnLogin').click()
-    cy.wait("@login")
+    cy.get('#logout').should("be.visible")
     cy.get('#logout').click();
   })
   it('logs in with user DEMO', function() {
     cy.visit('/')
+    cy.get('#txtEmail').should("be.visible")
     cy.get('#txtEmail').type('DEMO')
     cy.get('#txtPassword').type('demo')
     cy.get('#btnLogin').click()
-    cy.server()
-    cy.route("POST", "/api/serverSessionManager.asmx/GetVersion", []).as("login")
-    cy.wait("@login")
+    cy.get('#logout').should("be.visible")
     cy.get('#logout').click();
   })
   it('fails with wrong password', function() {
     cy.visit('/')
+    cy.get('#txtEmail').should("be.visible")
     cy.get('#txtEmail').type('DEMO')
     cy.get('#txtPassword').type('demo1234')
     cy.get('#btnLogin').click()
