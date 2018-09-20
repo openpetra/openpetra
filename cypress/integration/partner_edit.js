e04f3013cffa7f9310eb60b981d6e9e925e981db
Timotheus Pokorra <tp@tbits.net>
2018-09-20 21:42:01 +0200

    more fixes for the partner find and edit test

--- cypress/integration/partner_edit.js
+++ cypress/integration/partner_edit.js
@@ -6,6 +6,7 @@ describe('find and edit partner', function() {
     cy.get('#txtPassword').type('demo')
     cy.get('#btnLogin').click()
     cy.get('#logout').should("be.visible")
+
     cy.get('#btnFilter').click()
     cy.get('input[name="AFamilyNameOrOrganisation"]').type('jacob')
     cy.get('#btnSearch').click()
@@ -14,8 +15,12 @@ describe('find and edit partner', function() {
     cy.get('#modal_space input[name="PFamily_p_first_name_c"]').should('have.value', "Holger")
     cy.get('#modal_space input[name="PFamily_p_first_name_c"]').clear().type('Albrecht')
     cy.get('#modal_space #btnSave').click()
-    cy.get('#message').should("be.visible")
-    cy.get('#message').should("contain", 'Successfully saved')
+    cy.get('#message').should("be.visible").should("contain", 'Successfully saved')
+
+    cy.visit('/Partner/Partners/Maintain/MaintainPartners')
+    cy.get('#btnFilter').click()
+    cy.get('input[name="AFamilyNameOrOrganisation"]').type('jacob')
+    cy.get('#btnSearch').click()
     cy.get('#partner43012945').should("be.visible").click()
     cy.get('#partner43012945 #btnEdit').should("be.visible").click()
     cy.get('#modal_space input[name="PFamily_p_first_name_c"]').should('have.value', "Albrecht")
