d88cf8363220660af31f48e9005888eb5655159c
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2019-06-04 21:27:11 +0200

    need to use timestamp instead of datetime, because Postgresql does not have datetime

--- csharp/ICT/BuildTools/GenerateORM/codeGenerationPetra.cs
+++ csharp/ICT/BuildTools/GenerateORM/codeGenerationPetra.cs
@@ -82,7 +82,7 @@ namespace Ict.Tools.CodeGeneration.DataStore
             {
                 return "OdbcType.Date";
             }
-            else if (tableField.strType == "timestamp" || tableField.strType == "datetime")
+            else if (tableField.strType == "timestamp")
             {
                 return "OdbcType.DateTime";
             }
