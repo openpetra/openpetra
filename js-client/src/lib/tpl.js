ba5e9566e5dcca7be58a5eb4c0abf232f4997223
CJ <cj@tbits.net>
2019-12-06 13:59:24 +0100

    added global function to Date Object so it inputs today in a input field

--- js-client/src/lib/tpl.js
+++ js-client/src/lib/tpl.js
@@ -281,3 +281,9 @@ function update_requireClass(object, class_) {
 	object.find('[requireClass]').hide();
 	object.find('.'+class_).show();
 }
+
+Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+});
