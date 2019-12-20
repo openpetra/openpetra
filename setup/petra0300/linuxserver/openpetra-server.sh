706bc6e161ecd7bba4aaf7c9b0cce32f3c009341
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2019-12-20 06:44:05 +0100

    openpetra script: show which yml.gz file is being loaded

--- setup/petra0300/linuxserver/openpetra-server.sh
+++ setup/petra0300/linuxserver/openpetra-server.sh
@@ -102,6 +102,7 @@ stop() {
 
 # load a new database from a yml.gz file. this will overwrite the current database!
 loadYmlGz() {
+    echo "loading database from $ymlgzfile"
     su $userName -c "cd $OpenPetraPathBin; mono --runtime=v4.0 Ict.Petra.Tools.MSysMan.YmlGzImportExport.exe -C:/home/$userName/etc/PetraServerConsole.config -Action:load -YmlGzFile:$ymlgzfile" || exit -1
 }
 
