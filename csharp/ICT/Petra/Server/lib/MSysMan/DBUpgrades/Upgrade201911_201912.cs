e1d3c88c8500fedb3a645bc05a4f8b80066462c3
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2019-12-31 10:17:17 +0100

    Upgrade to 2019.12 fix for motivation details, new details have not been activated by default. fixes #414

---
+++ csharp/ICT/Petra/Server/lib/MSysMan/DBUpgrades/Upgrade201911_201912.cs
@@ -0,0 +1,57 @@
+//
+// DO NOT REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+//
+// @Authors:
+//       timop
+//
+// Copyright 2004-2019 by OM International
+//
+// This file is part of OpenPetra.org.
+//
+// OpenPetra.org is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenPetra.org is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with OpenPetra.org.  If not, see <http://www.gnu.org/licenses/>.
+//
+using System;
+using System.Data;
+using Ict.Common.DB;
+using Ict.Common;
+using Ict.Common.Data;
+
+namespace Ict.Petra.Server.MSysMan.DBUpgrades
+{
+    /// <summary>
+    /// Upgrade the database
+    /// </summary>
+    public static partial class TDBUpgrade
+    {
+        /// Upgrade to version 2019-12
+        public static bool UpgradeDatabase201911_201912(TDataBase ADataBase)
+        {
+            TDBTransaction SubmitChangesTransaction = new TDBTransaction();
+            bool SubmitOK = false;
+
+            ADataBase.WriteTransaction(ref SubmitChangesTransaction,
+                ref SubmitOK,
+                delegate
+                {
+                    // fix for motivation details, new details have not been activated
+                    string stmt = "update a_motivation_detail set a_motivation_status_l = 1;"
+                    ADataBase.ExecuteNonQuery(stmt, SubmitChangesTransaction);
+
+                    SubmitOK = true;
+                });
+
+            return true;
+        }
+    }
+}
