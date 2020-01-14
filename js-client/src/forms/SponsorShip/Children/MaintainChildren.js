a9bef1e9aa89a97b9cc3b668fe5761cdb1bc86ec
CJ <cj@tbits.net>
2020-01-14 15:07:43 +0100

    main file generation and style change

---
+++ js-client/src/forms/SponsorShip/Children/MaintainChildren.js
@@ -0,0 +1,15 @@
+var MaintainChildren = new (class {
+  constructor() {
+
+  }
+
+  filterShow() {
+      // get infos from the filter ans search with them
+      console.log("OK");
+  }
+
+  show(filter={}) {
+      // get data from server and show them
+
+  }
+})
