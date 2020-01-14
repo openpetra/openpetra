f7c3687cf07c86473ac9815a49c0b4a94f27f59d
CJ <cj@tbits.net>
2020-01-14 16:23:54 +0100

    call to detail

--- js-client/src/forms/SponsorShip/Children/MaintainChildren.js
+++ js-client/src/forms/SponsorShip/Children/MaintainChildren.js
@@ -10,32 +10,40 @@ var MaintainChildren = new (class {
   filterShow() {
       // get infos from the filter ans search with them
       var filter = extract_data($("#filter"));
-      console.log(filter);
+      this.show(filter);
   }
 
   show(filter={}) {
-      // get data from server and show them
+    // get data from server and show them
 
-      var req = {
-        "AFirstName": filter.AFirstName ? filter.AFirstName : "",
-        "AFamilyName": filter.AFamilyName ? filter.AFamilyName : "",
-        "APartnerStatus": filter.APartnerStatus ? filter.APartnerStatus : "",
-        "ASponsorshipStatus": filter.ASponsorshipStatus ? filter.ASponsorshipStatus : "",
-        "ASponsorAdmin": filter.ASponsorAdmin ? filter.ASponsorAdmin : "",
-      };
+    var req = {
+      "AFirstName": filter.AFirstName ? filter.AFirstName : "",
+      "AFamilyName": filter.AFamilyName ? filter.AFamilyName : "",
+      "APartnerStatus": filter.APartnerStatus ? filter.APartnerStatus : "",
+      "ASponsorshipStatus": filter.ASponsorshipStatus ? filter.ASponsorshipStatus : "",
+      "ASponsorAdmin": filter.ASponsorAdmin ? filter.ASponsorAdmin : "",
+    };
 
-      api.post('serverMSponsorship.asmx/TSponsorshipWebConnector_FindChildren', req).then(
-        function (data) {
-          var parsed = JSON.parse(data.data.d);
+    api.post('serverMSponsorship.asmx/TSponsorshipWebConnector_FindChildren', req).then(
+      function (data) {
+        var parsed = JSON.parse(data.data.d);
 
-          var List = $("#result").html("");
-          for (var entry of parsed.result) {
-            var Copy = $("[phantom] .children").clone();
-            insertData(Copy, entry);
-            List.append(Copy);
-          }
+        var List = $("#result").html("");
+        for (var entry of parsed.result) {
+          var Copy = $("[phantom] .children").clone();
+          insertData(Copy, entry);
+          List.append(Copy);
         }
-      )
+      }
+    )
+  }
+
+  detail(HTMLButtom) {
+    var partner_key = $(HTMLButtom).closest(".row").find("[name=p_partner_key_n]").val();
+    console.log(partner_key);
 
   }
+
+
+
 })
