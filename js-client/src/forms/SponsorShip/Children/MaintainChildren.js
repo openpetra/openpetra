1c93879190d56aa9a1081aa46dd8be87a26818b0
CJ <cj@tbits.net>
2020-01-14 16:20:14 +0100

    base listing and edit link

--- js-client/src/forms/SponsorShip/Children/MaintainChildren.js
+++ js-client/src/forms/SponsorShip/Children/MaintainChildren.js
@@ -1,3 +1,7 @@
+$("document").ready(function () {
+  MaintainChildren.show();
+});
+
 var MaintainChildren = new (class {
   constructor() {
 
@@ -5,11 +9,33 @@ var MaintainChildren = new (class {
 
   filterShow() {
       // get infos from the filter ans search with them
-      console.log("OK");
+      var filter = extract_data($("#filter"));
+      console.log(filter);
   }
 
   show(filter={}) {
       // get data from server and show them
+
+      var req = {
+        "AFirstName": filter.AFirstName ? filter.AFirstName : "",
+        "AFamilyName": filter.AFamilyName ? filter.AFamilyName : "",
+        "APartnerStatus": filter.APartnerStatus ? filter.APartnerStatus : "",
+        "ASponsorshipStatus": filter.ASponsorshipStatus ? filter.ASponsorshipStatus : "",
+        "ASponsorAdmin": filter.ASponsorAdmin ? filter.ASponsorAdmin : "",
+      };
+
+      api.post('serverMSponsorship.asmx/TSponsorshipWebConnector_FindChildren', req).then(
+        function (data) {
+          var parsed = JSON.parse(data.data.d);
+
+          var List = $("#result").html("");
+          for (var entry of parsed.result) {
+            var Copy = $("[phantom] .children").clone();
+            insertData(Copy, entry);
+            List.append(Copy);
+          }
+        }
+      )
 
   }
 })
