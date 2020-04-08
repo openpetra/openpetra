99ce45afb1ff786ce6de37c2567813e14d8072bb
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2020-04-08 05:40:45 +0200

    sponsorship: add new values to tables in db upgrade

---
+++ csharp/ICT/Petra/Server/lib/MSysMan/DBUpgrades/Upgrade202003_202004.sql
@@ -0,0 +1,8 @@
+INSERT INTO p_type(p_type_code_c, p_type_description_c) VALUES('BOARDING_SCHOOL','Child at school');
+INSERT INTO p_type(p_type_code_c, p_type_description_c) VALUES('CHILDREN_HOME,Child at Orphanage');
+INSERT INTO p_type(p_type_code_c, p_type_description_c) VALUES('HOME_BASED','Child at home');
+INSERT INTO p_type(p_type_code_c, p_type_description_c) VALUES('PREVIOUS_CHILD','Previous child');
+INSERT INTO p_type(p_type_code_c, p_type_description_c) VALUES('CHILD_DIED','Child passed away');
+INSERT INTO s_module(s_module_id_c,s_module_name_c) VALUES('PARTNERVIEW','View and select Partners');
+INSERT INTO s_module(s_module_id_c,s_module_name_c) VALUES('SPONSORADMIN','Manage Sponsorships');
+INSERT INTO s_module(s_module_id_c,s_module_name_c) VALUES('SPONSORVIEW','View Sponsorships');
