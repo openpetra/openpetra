7d19d8bde8297e71b3879dd4c41615622f4ab4be
CJ <cj@tbits.net>
2020-04-09 13:42:22 +0200

    fixed a rename error

--- csharp/ICT/Petra/Server/lib/MPartner/web/Partner.DataHistory.cs
+++ csharp/ICT/Petra/Server/lib/MPartner/web/Partner.DataHistory.cs
@@ -31,6 +31,7 @@ using System.Collections.Generic;
 using Newtonsoft.Json;
 using Ict.Petra.Server.App.Core.Security;
 using Ict.Common;
+using System.Data.Odbc;
 
 namespace Ict.Petra.Server.MPartner.Partner.WebConnectors {
 
@@ -40,18 +41,61 @@ namespace Ict.Petra.Server.MPartner.Partner.WebConnectors {
         /// Returns all consents entrys for a given partner 
         /// </summary>
         [RequireModulePermission("PTNRUSER")]
-        public static DataConsetTDS GetHistory(
+        public static DataConsentTDS LastKnownEntry(
+            Int64 APartnerKey,
+            string ADataType
+        )
+        {
+            TDBTransaction T = new TDBTransaction();
+            TDataBase DB = DBAccess.Connect("Get Consent Channel + Purpose");
+            DataConsentTDS Set = new DataConsentTDS();
+            List<OdbcParameter> SQLParameter = new List<OdbcParameter>();
+
+            DB.ReadTransaction(ref T, delegate {
+
+                string sql = "SELECT " +
+                    "`p_data_history`.*, " +
+                    "GROUP_CONCAT(`p_data_history_permission`.`p_purpose_code_c` SEPARATOR ',') AS `AllowedPurposes` " +
+                	"FROM `p_data_history` " +
+                    "LEFT JOIN `p_data_history_permission` " +
+                    "ON `p_data_history`.`p_entry_id_i` = `p_data_history_permission`.`p_data_history_entry_i` " +
+                    "WHERE `p_data_history_permission`.`p_data_chancled_d` IS NULL " +
+                    "AND `p_data_history`.`p_partner_key_n` = ? " +
+                    "AND `p_data_history`.`p_type_c` = ?" +
+                    "GROUP BY `p_data_history`.`p_entry_id_i` " +
+                    "ORDER BY `p_data_history`.`p_entry_id_i` DESC " +
+                    "LIMIT 1";
+
+                SQLParameter.Add(new OdbcParameter("PartnerKey", OdbcType.VarChar) { Value = APartnerKey.ToString() } );
+                SQLParameter.Add(new OdbcParameter("DataType", OdbcType.VarChar) { Value = ADataType } );
+
+                // DB.SelectDT(Set, sql, T, SQLParameter.ToArray(), 0,0);
+                DB.SelectDT(Set.PDataHistory, sql, T, SQLParameter.ToArray()); 
+
+                PConsentChannelAccess.LoadAll(Set, T);
+                PPurposeAccess.LoadAll(Set, T);
+
+            });
+
+            return new DataConsentTDS();
+        }
+
+        /// <summary>
+        /// Returns all consents entrys for a given partner 
+        /// </summary>
+        [RequireModulePermission("PTNRUSER")]
+        public static DataConsentTDS GetHistory(
             Int64 APartnerKey
         )
         {
-            return new DataConsetTDS();
+            return new DataConsentTDS();
         }
 
         [RequireModulePermission("PTNRUSER")]
-        public static DataConsetTDS GetConsentChannelAndPurpose() {
+        public static DataConsentTDS GetConsentChannelAndPurpose() {
             TDBTransaction T = new TDBTransaction();
             TDataBase DB = DBAccess.Connect("Get Consent Channel + Purpose");
-            DataConsetTDS Set = new DataConsetTDS();
+            DataConsentTDS Set = new DataConsentTDS();
 
             DB.ReadTransaction(ref T, delegate {
 
@@ -88,7 +132,7 @@ namespace Ict.Petra.Server.MPartner.Partner.WebConnectors {
             // data is comming like this: ["{}", "{}"] 
             foreach (string JsonObjectString in JsonChanges) {
                 DataHistoryChange ChangeObject = JsonConvert.DeserializeObject<DataHistoryChange>(JsonObjectString);
-                DataConsetTDS Set = new DataConsetTDS();
+                DataConsentTDS Set = new DataConsentTDS();
 
                 // generate and add new row for p_data_history
                 PDataHistoryRow NewRow = Set.PDataHistory.NewRowTyped();
@@ -112,7 +156,7 @@ namespace Ict.Petra.Server.MPartner.Partner.WebConnectors {
                     Set.PDataHistoryPermission.Rows.Add(NewPermRow);
                 }
 
-                DataConsetTDSAccess.SubmitChanges(Set);
+                DataConsentTDSAccess.SubmitChanges(Set);
             }
 
             return;
