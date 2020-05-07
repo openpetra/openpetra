19b98f8003331437187d0e955f04b5dd394559d0
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2020-05-07 22:25:38 +0200

    initdb: avoid issue ERROR 1698 (28000): Access denied for user 'root'@'localhost' when connecting to 127.0.0.1 related to #471

--- setup/petra0300/linuxserver/openpetra-server.sh
+++ setup/petra0300/linuxserver/openpetra-server.sh
@@ -505,10 +505,10 @@ mysqlinitdb() {
       echo "USE \`$OPENPETRA_DBNAME\`;" >> $OpenPetraPath/tmp/createdb-MySQL.sql
       if [ ! -z "$MYSQL_ROOT_PWD" ]; then 
         echo "GRANT ALL ON \`$OPENPETRA_DBNAME\`.* TO \`$OPENPETRA_DBUSER\`@'%' IDENTIFIED BY '$OPENPETRA_DBPWD'" >> $OpenPetraPath/tmp/createdb-MySQL.sql
-        mysql -u root --host=$OPENPETRA_DBHOST --port=$OPENPETRA_DBPORT --password="$MYSQL_ROOT_PWD" < $OpenPetraPath/tmp/createdb-MySQL.sql || exit -1
+        mysql -u root --port=$OPENPETRA_DBPORT --password="$MYSQL_ROOT_PWD" < $OpenPetraPath/tmp/createdb-MySQL.sql || exit -1
       else
         echo "GRANT ALL ON \`$OPENPETRA_DBNAME\`.* TO \`$OPENPETRA_DBUSER\`@127.0.0.1 IDENTIFIED BY '$OPENPETRA_DBPWD'" >> $OpenPetraPath/tmp/createdb-MySQL.sql
-        mysql -u root --host=$OPENPETRA_DBHOST --port=$OPENPETRA_DBPORT < $OpenPetraPath/tmp/createdb-MySQL.sql || exit -1
+        mysql -u root --port=$OPENPETRA_DBPORT < $OpenPetraPath/tmp/createdb-MySQL.sql || exit -1
       fi
       rm -f $OpenPetraPath/tmp/createdb-MySQL.sql
     fi
