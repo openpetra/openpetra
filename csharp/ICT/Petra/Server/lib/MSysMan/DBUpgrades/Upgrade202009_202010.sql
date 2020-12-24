28bbdc319f073a131861091f240314b1af5f028d
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2020-12-24 13:55:24 +0100

    fix for Postgresql

--- csharp/ICT/Petra/Server/lib/MSysMan/DBUpgrades/Upgrade202009_202010.sql
+++ csharp/ICT/Petra/Server/lib/MSysMan/DBUpgrades/Upgrade202009_202010.sql
@@ -1,3 +1,3 @@
-insert into p_type_category(p_code_c, p_description_c, p_deletable_flag_l) values('SPONSORED_CHILD_STATUS', 'Status Options for sponsored child', 0);
+insert into p_type_category(p_code_c, p_description_c, p_deletable_flag_l) values('SPONSORED_CHILD_STATUS', 'Status Options for sponsored child', False);
 update p_type set p_category_code_c = 'SPONSORED_CHILD_STATUS' WHERE p_type_code_c IN ('BOARDING_SCHOOL', 'CHILDREN_HOME', 'CHILD_DIED', 'HOME_BASED', 'PREVIOUS_CHILD');
 
