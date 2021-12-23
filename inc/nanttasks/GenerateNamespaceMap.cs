20ee35d853c398c3d42b07ba8742b51e80fd544e
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2021-12-23 22:19:04 +0100

    nuget mailkit: use net46 packages

--- inc/nanttasks/GenerateNamespaceMap.cs
+++ inc/nanttasks/GenerateNamespaceMap.cs
@@ -474,7 +474,7 @@ namespace Ict.Tools.NAntTasks
                             string dllPath = pkgName + "." + NuPackages[pkgName];
 
                             // if another path is added here, also add in ThirdParty.build
-                            string[] netVersionPaths = new string[] {"/lib/net461", "/lib/net45", "/lib/net40", "/lib/Net40", "/lib/net5.0", "/lib/net20" };
+                            string[] netVersionPaths = new string[] {"/lib/net461", "/lib/net46", "/lib/net45", "/lib/net40", "/lib/Net40", "/lib/net5.0", "/lib/net20" };
                             foreach (string netVersionPath in netVersionPaths)
                             {
                                 if (Directory.Exists(NugetPackagesPath + PSC + dllPath + netVersionPath))
