eb1ad83e9fbe0a94eb309885023fe671123ace7e
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2025-01-01 23:30:15 +0100

    MaintainPublications: edit active flag

--- csharp/ICT/Petra/Server/lib/MPartner/web/ModuleSetup.cs
+++ csharp/ICT/Petra/Server/lib/MPartner/web/ModuleSetup.cs
@@ -4,7 +4,7 @@
 // @Authors:
 //       timop
 //
-// Copyright 2004-2024 by OM International
+// Copyright 2004-2025 by OM International
 //
 // This file is part of OpenPetra.org.
 //
@@ -503,7 +503,7 @@ namespace Ict.Petra.Server.MPartner.TableMaintenance.WebConnectors
         /// maintain publications
         /// </summary>
         [RequireModulePermission("PTNRADMIN")]
-        public static bool MaintainPublications(string action, string APublicationCode, string APublicationDescription, out TVerificationResultCollection AVerificationResult)
+        public static bool MaintainPublications(string action, string APublicationCode, string APublicationDescription, bool AValidPublication, out TVerificationResultCollection AVerificationResult)
         {
             PartnerSetupTDS MainDS = new PartnerSetupTDS();
             AVerificationResult = new TVerificationResultCollection();
@@ -514,6 +514,7 @@ namespace Ict.Petra.Server.MPartner.TableMaintenance.WebConnectors
                 row.PublicationCode = APublicationCode.ToUpper();
                 row.PublicationDescription = APublicationDescription;
                 row.FrequencyCode = "Annual";
+                row.ValidPublication = true;
                 MainDS.PPublication.Rows.Add(row);
                 try
                 {
@@ -533,6 +534,7 @@ namespace Ict.Petra.Server.MPartner.TableMaintenance.WebConnectors
                     if (row.PublicationCode == APublicationCode)
                     {
                         row.PublicationDescription = APublicationDescription;
+                        row.ValidPublication = AValidPublication;
                     }
                 }
 
